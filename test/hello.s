d2e5d9db030f5ad48d82d68e433691685c4d3092
Rui Ueyama <ruiu@cs.stanford.edu>
2020-11-03 22:57:37 +0900

    temporary

---
+++ test/hello.s
@@ -0,0 +1,24 @@
+// RUN: cc -o %t.o -c %s
+// RUN: mold -o %t.exe /usr/lib/x86_64-linux-gnu/crt1.o \
+// RUN:   /usr/lib/x86_64-linux-gnu/crti.o \
+// RUN:   /usr/lib/gcc/x86_64-linux-gnu/9/crtbeginT.o \
+// RUN:   %t.o \
+// RUN:   /usr/lib/gcc/x86_64-linux-gnu/9/libgcc.a \
+// RUN:   /usr/lib/gcc/x86_64-linux-gnu/9/libgcc_eh.a \
+// RUN:   /usr/lib/x86_64-linux-gnu/libc.a \
+// RUN:   /usr/lib/gcc/x86_64-linux-gnu/9/crtend.o \
+// RUN:   /usr/lib/x86_64-linux-gnu/crtn.o
+// RUN: %t.exe
+
+        .text
+        .globl main
+main:
+        lea msg(%rip), %rdi
+        xor %rax, %rax
+        call printf
+        xor %rax, %rax
+        ret
+
+        .data
+msg:
+        .string "Hello world\n"
