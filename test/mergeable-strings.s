d29b17cf0a2bf449394bd4b3a19b7544e1441d68
Rui Ueyama <ruiu@cs.stanford.edu>
2020-11-07 18:18:13 +0900

    temporary

---
+++ test/mergeable-strings.s
@@ -0,0 +1,27 @@
+// RUN: cc -o %t.o -c %s
+// RUN: mold -static -o %t.exe /usr/lib/x86_64-linux-gnu/crt1.o \
+// RUN:   /usr/lib/x86_64-linux-gnu/crti.o \
+// RUN:   /usr/lib/gcc/x86_64-linux-gnu/9/crtbeginT.o \
+// RUN:   %t.o \
+// RUN:   /usr/lib/gcc/x86_64-linux-gnu/9/libgcc.a \
+// RUN:   /usr/lib/gcc/x86_64-linux-gnu/9/libgcc_eh.a \
+// RUN:   /usr/lib/x86_64-linux-gnu/libc.a \
+// RUN:   /usr/lib/gcc/x86_64-linux-gnu/9/crtend.o \
+// RUN:   /usr/lib/x86_64-linux-gnu/crtn.o
+// RUN: %t.exe | grep 'Hello world'
+
+        .text
+        .globl main
+main:
+        lea .rodata.str1.1(%rip), %rdi
+        xor %rax, %rax
+        call printf
+        lea .rodata.str1.1+9(%rip), %rdi
+        xor %rax, %rax
+        call printf
+        xor %rax, %rax
+        ret
+
+        .section .rodata.str1.1, "aMS", @progbits, 1
+        .string "Hello"
+        .string "foo world\n"
