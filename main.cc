b14e03db020a79b19b62fa02c1e8c17fdd21fd8c
Rui Ueyama <ruiu@cs.stanford.edu>
2020-11-19 17:10:58 +0900

    temporary

--- main.cc
+++ main.cc
@@ -753,9 +753,9 @@ int main(int argc, char **argv) {
   out::chunks.push_back(out::shdr);
 
   // Set section indices.
-  for (int i = 0; i < out::chunks.size(); i++)
+  for (int i = 0, shndx = 1; i < out::chunks.size(); i++)
     if (out::chunks[i]->kind != OutputChunk::HEADER)
-      out::chunks[i]->shndx = i + 1;
+      out::chunks[i]->shndx = shndx++;
 
   // Now that we have computed sizes for all sections and assigned
   // section indices to them, so we can fix section header contents
