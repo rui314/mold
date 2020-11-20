5e14f2394ff960270de14f8399a53b5ef7f05628
Rui Ueyama <ruiu@cs.stanford.edu>
2020-11-20 14:53:52 +0900

    temporary

--- test/dynamic.s
+++ test/dynamic.s
@@ -11,41 +11,37 @@
 // RUN:   /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2
 
 // RUN: readelf --dynamic %t.exe | FileCheck %s
-// CHECK: Dynamic section at offset 0x2080 contains 22 entries:
+// CHECK: Dynamic section at offset 0x2048 contains 22 entries:
 // CHECK:   Tag        Type                         Name/Value
 // CHECK:  0x0000000000000001 (NEEDED)             Shared library: [libgcc_s.so.1]
 // CHECK:  0x0000000000000001 (NEEDED)             Shared library: [libc.so.6]
 // CHECK:  0x0000000000000001 (NEEDED)             Shared library: [ld-linux-x86-64.so.2]
-// CHECK:  0x0000000000000007 (RELA)               0x2003a0
-// CHECK:  0x0000000000000008 (RELASZ)             120 (bytes)
+// CHECK:  0x0000000000000007 (RELA)               0x2001c8
+// CHECK:  0x0000000000000008 (RELASZ)             48 (bytes)
 // CHECK:  0x0000000000000009 (RELAENT)            24 (bytes)
-// CHECK:  0x0000000000000017 (JMPREL)             0x200370
-// CHECK:  0x0000000000000002 (PLTRELSZ)           48 (bytes)
-// CHECK:  0x0000000000000003 (PLTGOT)             0x202058
+// CHECK:  0x0000000000000017 (JMPREL)             0x2001b0
+// CHECK:  0x0000000000000002 (PLTRELSZ)           24 (bytes)
+// CHECK:  0x0000000000000003 (PLTGOT)             0x202028
 // CHECK:  0x0000000000000014 (PLTREL)             RELA
-// CHECK:  0x0000000000000006 (SYMTAB)             0x200418
+// CHECK:  0x0000000000000006 (SYMTAB)             0x2001f8
 // CHECK:  0x000000000000000b (SYMENT)             24 (bytes)
-// CHECK:  0x0000000000000005 (STRTAB)             0x2004d8
-// CHECK:  0x000000000000000a (STRSZ)              129 (bytes)
-// CHECK:  0x0000000000000004 (HASH)               0x20055c
-// CHECK:  0x0000000000000019 (INIT_ARRAY)         0x202028
+// CHECK:  0x0000000000000005 (STRTAB)             0x200258
+// CHECK:  0x000000000000000a (STRSZ)              86 (bytes)
+// CHECK:  0x0000000000000004 (HASH)               0x2002b0
+// CHECK:  0x0000000000000019 (INIT_ARRAY)         0x2021d0
 // CHECK:  0x000000000000001b (INIT_ARRAYSZ)       8 (bytes)
-// CHECK:  0x000000000000001a (FINI_ARRAY)         0x202020
+// CHECK:  0x000000000000001a (FINI_ARRAY)         0x2021c8
 // CHECK:  0x000000000000001c (FINI_ARRAYSZ)       8 (bytes)
-// CHECK:  0x000000000000000c (INIT)               0x201010
-// CHECK:  0x000000000000000d (FINI)               0x201000
+// CHECK:  0x000000000000000c (INIT)               0x201030
+// CHECK:  0x000000000000000d (FINI)               0x201020
 // CHECK:  0x0000000000000000 (NULL)               0x0
 
 // RUN: readelf --symbols --use-dynamic %t.exe | FileCheck --check-prefix=DYNAMIC %s
 // DYNAMIC: Symbol table for image:
 // DYNAMIC:   Num Buc:    Value          Size   Type   Bind Vis      Ndx Name
-// DYNAMIC:    7   1: 00000000002011a0     0 FUNC    GLOBAL DEFAULT  13 __libc_csu_fini
-// DYNAMIC:    6   4: 0000000000201130     0 FUNC    GLOBAL DEFAULT  13 __libc_csu_init
-// DYNAMIC:    2   4: 0000000000201010     0 FUNC    GLOBAL DEFAULT  12 _init
-// DYNAMIC:    5   6: 0000000000000000     0 FUNC    GLOBAL DEFAULT UND __libc_start_main
-// DYNAMIC:    4   6: 0000000000000000     0 FUNC    GLOBAL DEFAULT UND printf
-// DYNAMIC:    3   6: 0000000000201116     0 NOTYPE  GLOBAL DEFAULT  13 main
-// DYNAMIC:    1   7: 0000000000000000     0 NOTYPE  WEAK   DEFAULT UND __gmon_start__
+// DYNAMIC:     3   2: 0000000000000000     0 FUNC    GLOBAL DEFAULT UND __libc_start_main
+// DYNAMIC:     2   2: 0000000000000000     0 FUNC    GLOBAL DEFAULT UND printf
+// DYNAMIC:     1   3: 0000000000000000     0 NOTYPE  WEAK   DEFAULT UND __gmon_start__
 
         .globl main
 main:
--- test/plt.s
+++ test/plt.s
@@ -12,18 +12,26 @@
 // RUN:   /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2
 
 // RUN: readelf --sections %t.exe | FileCheck --check-prefix=SECTIONS %s
-// SECTIONS: [19] .got.plt          PROGBITS         0000000000202058  00002058
-// SECTIONS:      0000000000000028  0000000000000000  WA       0     0     8
-// SECTIONS: [20] .dynamic          DYNAMIC          0000000000202080  00002080
-// SECTIONS:      0000000000000160  0000000000000010  WA       9     0     8
+// SECTIONS:   [15] .got              PROGBITS         0000000000202000  00002000
+// SECTIONS:        0000000000000028  0000000000000000  WA       0     0     8
+// SECTIONS:   [16] .got.plt          PROGBITS         0000000000202028  00002028
+// SECTIONS:        0000000000000020  0000000000000000  WA       0     0     8
 
 // RUN: readelf -x .got.plt %t.exe | FileCheck --check-prefix=GOTPLT %s
-// GOTPLT: 0x00202058 80202000 00000000 00000000 00000000
+// GOTPLT: Hex dump of section '.got.plt':
+// GOTPLT:   0x00202028 48202000 00000000 00000000 00000000 H  .............
+// GOTPLT:   0x00202038 00000000 00000000 16102000 00000000 .......... .....
 
 // RUN: objdump -d -j .plt %t.exe | FileCheck --check-prefix=PLT %s
-// PLT: 2011a8: ff 35 b2 0e 00 00       pushq  0xeb2(%rip)   # 202060
-// PLT: 2011ae: ff 25 b4 0e 00 00       jmpq   *0xeb4(%rip)  # 202068
-// PLT: 2011b4: 0f 1f 40 00             nopl   0x0(%rax)
+// PLT: Disassembly of section .plt:
+// PLT: 0000000000201000 <printf@plt-0x10>:
+// PLT:   201000:       ff 35 2a 10 00 00       pushq  0x102a(%rip)        # 202030 <_GLOBAL_OFFSET_TABLE_+0x8>
+// PLT:   201006:       ff 25 2c 10 00 00       jmpq   *0x102c(%rip)        # 202038 <_GLOBAL_OFFSET_TABLE_+0x10>
+// PLT:   20100c:       0f 1f 40 00             nopl   0x0(%rax)
+// PLT: 0000000000201010 <printf@plt>:
+// PLT:   201010:       ff 25 2a 10 00 00       jmpq   *0x102a(%rip)        # 202040 <printf>
+// PLT:   201016:       68 00 00 00 00          pushq  $0x0
+// PLT:   20101b:       e9 e0 ff ff ff          jmpq   201000 <_IO_stdin_used+0xb70>
 
         .text
         .globl main
