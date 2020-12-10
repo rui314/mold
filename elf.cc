f7a79b5ef24b5a9b37842619ef14d24d3918dcd9
Rui Ueyama <ruiu@cs.stanford.edu>
2020-12-10 17:53:37 +0900

    temporary

--- elf.cc
+++ elf.cc
@@ -8,9 +8,19 @@ ElfFile::ElfFile(MemoryMappedFile mb) : mb(mb), ehdr((ElfEhdr &)*mb.data) {
 }
 
 std::span<ElfShdr> ElfFile::get_sections() const {
+  u8 *begin = mb.data + ehdr.e_shoff;
+  u8 *end = begin + ehdr.e_shnum * sizeof(ElfShdr);
+  if (mb.data + mb.size < end)
+    error(mb.name + ": e_shoff or e_shnum corrupted");
+  return {(ElfShdr *)begin, (ElfShdr *)end};
 }
 
 std::span<ElfSym> ElfFile::get_symbols(const ElfShdr &shdr) const {
+  u8 *begin = mb.data + shdr.sh_offset;
+  u8 *end = begin + shdr.sh_size;
+  if (mb.data + mb.size < end)
+    error(mb.name + ": symbol shdr corrupted");
+  return {(ElfSym *)begin, (ElfSym *)end};
 }
 
 std::string_view ElfFile::get_section_name(const ElfShdr &shdr) {
