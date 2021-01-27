6d9e3ffddb129ee64cdd838da91b3854edf9482e
Rui Ueyama <ruiu@cs.stanford.edu>
2021-01-27 19:21:29 +0900

    wip

--- icf.cc
+++ icf.cc
@@ -1,5 +1,6 @@
 #include "mold.h"
 
+#include <openssl/sha.h>
 #include <tbb/parallel_for.h>
 #include <tbb/parallel_for_each.h>
 
@@ -12,8 +13,32 @@ static bool is_eligible(InputSection &isec) {
          !(isec.shdr.sh_type == SHT_FINI_ARRAY || isec.name == ".fini");
 }
 
-static void compute_signature(InputSection &isec, u8 *digest) {
-  memset(digest, 0, HASH_SIZE);
+static void compute_digest(InputSection &isec, u8 *digest) {
+  SHA256_CTX ctx;
+  SHA256_Init(&ctx);
+
+  std::string_view contents = isec.get_contents();
+  SHA256_Update(&ctx, contents.data(), contents.size());
+  SHA256_Update(&ctx, &isec.shdr.sh_flags, sizeof(isec.shdr.sh_flags));
+
+  for (ElfRela &rel : isec.rels) {
+    SHA256_Update(&ctx, &rel.r_offset, sizeof(rel.r_offset));
+    SHA256_Update(&ctx, &rel.r_type, sizeof(rel.r_type));
+    SHA256_Update(&ctx, &rel.r_addend, sizeof(rel.r_addend));
+
+    Symbol &sym = *isec.file->symbols[rel.r_sym];
+
+    if (SectionFragment *frag = sym.fragref.frag) {
+      SHA256_Update(&ctx, frag->data.data(), frag->data.size());
+      SHA256_Update(&ctx, &sym.fragref.addend, sizeof(sym.fragref.addend));
+    } else if (!sym.input_section) {
+      SHA256_Update(&ctx, &sym.value, sizeof(sym.value));
+    }
+  }
+
+  u8 tmp[32];
+  assert(SHA256_Final(tmp, &ctx) == 1);
+  memcpy(digest, tmp, HASH_SIZE);
 }
 
 struct Pair {
@@ -28,7 +53,7 @@ static void gather_sections(std::vector<Pair> &eligibles,
 
   tbb::parallel_for((i64)0, (i64)out::objs.size(), [&](i64 i) {
     for (InputSection *isec : out::objs[i]->sections) {
-      if (!isec)
+      if (!isec || isec->shdr.sh_type == SHT_NOBITS)
         continue;
 
       if (is_eligible(*isec))
@@ -54,16 +79,16 @@ static void gather_sections(std::vector<Pair> &eligibles,
     i64 idx2 = non_eligible_indices[i];
 
     for (InputSection *isec : out::objs[i]->sections) {
-      if (!isec)
+      if (!isec || isec->shdr.sh_type == SHT_NOBITS)
         continue;
 
       if (is_eligible(*isec)) {
         eligibles[idx1].isec = isec;
-        compute_signature(*isec, eligibles[idx1].digest);
+        compute_digest(*isec, eligibles[idx1].digest);
         idx1++;
       } else {
         non_eligibles[idx2].isec = isec;
-        compute_signature(*isec, non_eligibles[idx2].digest);
+        compute_digest(*isec, non_eligibles[idx2].digest);
         idx2++;
       }
     }
