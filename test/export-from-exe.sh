f3c66028b46f25282af27d563626938fedce2e18
Rui Ueyama <ruiu@cs.stanford.edu>
2021-02-20 23:48:27 +0900

    wip

---
+++ test/export-from-exe.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+echo -n "Testing $(basename -s .sh $0) ... "
+t=$(pwd)/tmp/$(basename -s .sh $0)
+mkdir -p $t
+
+cat <<EOF | cc -o $t/a.o -c -x assembler -
+.globl _start, expfn
+_start:
+expfn:
+  nop
+EOF
+
+cat <<EOF | cc -shared -o $t/b.so -x assembler -
+.globl x
+x:
+  call expfn@PLT
+EOF
+
+../mold -o $t/exe $t/a.o $t/b.so
+
+echo OK
