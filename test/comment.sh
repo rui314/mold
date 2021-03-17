614ddb309a88c62ef1f0047b00e21196829d7dd1
Rui Ueyama <ruiu@cs.stanford.edu>
2021-03-17 22:56:04 +0900

    Add a test

---
+++ test/comment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+cd $(dirname $0)
+echo -n "Testing $(basename -s .sh $0) ... "
+t=$(pwd)/tmp/$(basename -s .sh $0)
+mkdir -p $t
+
+cat <<EOF | clang -c -o $t/a.o -xc -
+int main() {}
+EOF
+
+clang -fuse-ld=`pwd`/../mold -o $t/exe $t/a.o
+readelf -p .comment $t/exe | grep -q 'mold linker'
+
+echo OK
