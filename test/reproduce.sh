4dc0918a3451e033891849cf8305b8323c9a26da
Rui Ueyama <ruiu@cs.stanford.edu>
2021-04-10 15:33:51 +0900

    wip

---
+++ test/reproduce.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+cd $(dirname $0)
+echo -n "Testing $(basename -s .sh $0) ... "
+t=$(pwd)/tmp/$(basename -s .sh $0)
+mkdir -p $t
+
+cat <<EOF | clang -c -o $t/a.o -xc -
+#include <stdio.h>
+
+int main() {
+  printf("Hello world\n");
+  return 0;
+}
+EOF
+
+clang -fuse-ld=`pwd`/../mold -o $t/exe $t/a.o -Wl,-reproduce=$t/repro.tar
+
+tar -C $t -xf $t/repro.tar
+fgrep -q /a.o  $t/repro/response.txt
+fgrep -q mold $t/repro/version.txt
+
+echo OK
