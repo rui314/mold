c805f7c0878c837d821412eea50308c2bc194165
Rui Ueyama <ruiu@cs.stanford.edu>
2021-05-29 14:18:28 +0900

    Fix broken tests

--- test/dynamic.sh
+++ test/dynamic.sh
@@ -19,42 +19,12 @@ echo '.globl main; main:' | cc -o $t/a.o -c -x assembler -
   /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
   /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2
 
-readelf --dynamic $t/exe | grep -q "
-Dynamic section at offset 0x2048 contains 26 entries:
-  Tag        Type                         Name/Value
- 0x0000000000000001 (NEEDED)             Shared library: [libgcc_s.so.1]
- 0x0000000000000001 (NEEDED)             Shared library: [libc.so.6]
- 0x0000000000000001 (NEEDED)             Shared library: [ld-linux-x86-64.so.2]
- 0x0000000000000007 (RELA)               0x2001c8
- 0x0000000000000008 (RELASZ)             24 (bytes)
- 0x0000000000000009 (RELAENT)            24 (bytes)
- 0x0000000000000017 (JMPREL)             0x2001b0
- 0x0000000000000002 (PLTRELSZ)           24 (bytes)
- 0x0000000000000003 (PLTGOT)             0x202028
- 0x0000000000000014 (PLTREL)             RELA
- 0x0000000000000006 (SYMTAB)             0x2001e0
- 0x000000000000000b (SYMENT)             24 (bytes)
- 0x0000000000000005 (STRTAB)             0x200228
- 0x000000000000000a (STRSZ)              83 (bytes)
- 0x0000000000000004 (HASH)               0x20027c
- 0x0000000000000019 (INIT_ARRAY)         0x202210
- 0x000000000000001b (INIT_ARRAYSZ)       8 (bytes)
- 0x000000000000001a (FINI_ARRAY)         0x202208
- 0x000000000000001c (FINI_ARRAYSZ)       8 (bytes)
- 0x000000006ffffff0 (VERSYM)             0x20029c
- 0x000000006ffffffe (VERNEED)            0x2002a8
- 0x000000006fffffff (VERNEEDNUM)         1
- 0x0000000000000015 (DEBUG)              0x0
- 0x000000000000000c (INIT)               0x201030
- 0x000000000000000d (FINI)               0x201020
- 0x0000000000000000 (NULL)               0x0
-"
+readelf --dynamic $t/exe > $t/log
+fgrep -q 'Shared library: [libgcc_s.so.1]' $t/log
+fgrep -q 'Shared library: [libc.so.6]' $t/log
+fgrep -q 'Shared library: [ld-linux-x86-64.so.2]' $t/log
 
-readelf --symbols --use-dynamic $t/exe | grep -q "
-Symbol table for image:
-  Num Buc:    Value          Size   Type   Bind Vis      Ndx Name
-    1   1: 0000000000000000   483 FUNC    GLOBAL DEFAULT UND __libc_start_main
-    2   2: 0000000000000000   204 FUNC    GLOBAL DEFAULT UND printf
-"
+readelf --symbols --use-dynamic $t/exe > $t/log2
+fgrep -q 'FUNC    GLOBAL DEFAULT UND __libc_start_main' $t/log2
 
 echo OK
--- test/export-dynamic.sh
+++ test/export-dynamic.sh
@@ -22,28 +22,8 @@ EOF
 cc -shared -fPIC -o $t/b.so -xc /dev/null
 ../mold -o $t/exe $t/a.o $t/b.so --export-dynamic
 
-readelf --dyn-syms $t/exe | grep -q "
-Symbol table '.dynsym' contains 19 entries:
-   Num:    Value          Size Type    Bind   Vis      Ndx Name
-     0: 0000000000000000     0 NOTYPE  LOCAL  DEFAULT  UND
-     1: 0000000000201010     0 NOTYPE  GLOBAL DEFAULT    6 foo
-     2: 0000000000201011     0 NOTYPE  GLOBAL DEFAULT    6 bar
-     3: 0000000000201012     0 NOTYPE  GLOBAL DEFAULT    6 _start
-     4: 0000000000200000     0 NOTYPE  GLOBAL DEFAULT  ABS __ehdr_start
-     5: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __rela_iplt_start
-     6: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __rela_iplt_end
-     7: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __init_array_start
-     8: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __init_array_end
-     9: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __fini_array_start
-    10: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __fini_array_end
-    11: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __preinit_array_start
-    12: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __preinit_array_end
-    13: 0000000000202018     0 NOTYPE  GLOBAL DEFAULT  ABS _DYNAMIC
-    14: 0000000000202000     0 NOTYPE  GLOBAL DEFAULT  ABS _GLOBAL_OFFSET_TABLE_
-    15: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT  ABS __bss_start
-    16: 0000000000202178     0 NOTYPE  GLOBAL DEFAULT  ABS _end
-    17: 0000000000201013     0 NOTYPE  GLOBAL DEFAULT  ABS _etext
-    18: 0000000000202178     0 NOTYPE  GLOBAL DEFAULT  ABS _edata
-"
+readelf --dyn-syms $t/exe > $t/log
+fgrep -q 'NOTYPE  GLOBAL DEFAULT    6 bar' $t/log
+fgrep -q 'NOTYPE  GLOBAL DEFAULT    6 _start' $t/log
 
 echo OK
