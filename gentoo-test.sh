3bfefefb2d5b430c2eeb2eab994e0732d7069e51
Rui Ueyama <ruiu@cs.stanford.edu>
2021-06-02 20:23:47 +0900

    Kill build commands that ignore SIGTERM with SIGKILL

--- gentoo-test.sh
+++ gentoo-test.sh
@@ -44,7 +44,7 @@ build() {
   cmd="FEATURES=test MAKEOPTS=-j8 emerge $package"
   filename=`echo "$package" | sed 's!/!_!g'`
   link="ln -sf /mold/mold /usr/x86_64-pc-linux-gnu/bin/ld"
-  docker="timeout --verbose 10m docker run --rm --cap-add=SYS_PTRACE -v `pwd`:/mold mold-gentoo"
+  docker="docker run --rm --cap-add=SYS_PTRACE -v `pwd`:/mold mold-gentoo timeout -v -k 10s 10m"
   dir=gentoo/$git_hash
 
   mkdir -p $dir/success $dir/failure
