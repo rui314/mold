faee02060dfef6a4a2f0d5a28fbd893ebbff1701
Rui Ueyama <ruiu@cs.stanford.edu>
2021-06-15 13:06:43 +0900

    Improve test script

--- gentoo-test.sh
+++ gentoo-test.sh
@@ -24,7 +24,7 @@ if ! docker image ls mold-gentoo | grep -q mold-gentoo; then
   cat <<EOF | docker build -t mold-gentoo -
 FROM gentoo/stage3
 RUN emerge-webrsync
-RUN echo 'USE="elogind corefonts truetype jpeg2k tiff -systemd"' >> /etc/portage/make.conf && \
+RUN echo 'USE="elogind -systemd corefonts truetype jpeg jpeg2k tiff zstd static-libs"' >> /etc/portage/make.conf && \
     echo 'ACCEPT_LICENSE="* -@EULA"' >> /etc/portage/make.conf && \
     echo 'FEATURES="${FEATURE} noclean nostrip -ipc-sandbox -network-sandbox -pid-sandbox -sandbox"' >> /etc/portage/make.conf
 EOF
@@ -42,7 +42,7 @@ git_hash=$(./mold --version | perl -ne '/\((\w+)/; print $1;')
 # Build a given package in Docker
 build() {
   package="$1"
-  cmd="FEATURES=test MAKEOPTS=-j8 emerge $package"
+  cmd="MAKEOPTS=-j8 emerge --onlydeps $package && FEATURES=test MAKEOPTS=-j8 emerge $package"
   filename=`echo "$package" | sed 's!/!_!g'`
   link="ln -sf /mold/mold /usr/x86_64-pc-linux-gnu/bin/ld"
   docker="docker run --rm --cap-add=SYS_PTRACE -v `pwd`:/mold mold-gentoo timeout -v -k 10s 30m"
