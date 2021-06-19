04f82b4b13bbb78901c194e0e955c844e77c8cfb
Rui Ueyama <ruiu@cs.stanford.edu>
2021-06-20 00:55:26 +0900

    Fix Gentoo build script

--- gentoo-test.sh
+++ gentoo-test.sh
@@ -59,7 +59,7 @@ build() {
     mv $dir/$filename.mold $dir/failure
   fi
 
-  $docker bash -c "$cmd1 && cmd3" >& $dir/$filename.ld
+  $docker bash -c "$cmd1 && $cmd3" >& $dir/$filename.ld
   if [ $? = 0 ]; then
     mv $dir/$filename.ld $dir/success
   else
