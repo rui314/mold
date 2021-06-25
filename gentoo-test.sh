866a1dbec0d6e8c0ccb6fb4949cbdf60d1faf5af
Rui Ueyama <ruiu@cs.stanford.edu>
2021-06-25 13:55:40 +0900

    Improve Gentoo test script

--- gentoo-test.sh
+++ gentoo-test.sh
@@ -24,7 +24,7 @@ if ! docker image ls mold-gentoo | grep -q mold-gentoo; then
   cat <<EOF | docker build -t mold-gentoo -
 FROM gentoo/stage3
 RUN emerge-webrsync
-RUN echo 'USE="elogind -systemd corefonts truetype jpeg jpeg2k tiff zstd static-libs"' >> /etc/portage/make.conf && \
+RUN echo 'USE="elogind -systemd corefonts truetype jpeg jpeg2k tiff zstd static-libs binary"' >> /etc/portage/make.conf && \
     echo 'ACCEPT_LICENSE="* -@EULA"' >> /etc/portage/make.conf && \
     echo 'FEATURES="${FEATURE} noclean nostrip -ipc-sandbox -network-sandbox -pid-sandbox -sandbox"' >> /etc/portage/make.conf
 RUN emerge gdb lld vim strace && rm -rf /var/tmp/portage
@@ -43,9 +43,9 @@ git_hash=$(./mold --version | perl -ne '/\((\w+)/; print $1;')
 # Build a given package in Docker
 build() {
   package="$1"
-  cmd1="MAKEOPTS=-j8 emerge --onlydeps $package"
+  cmd1="MAKEOPTS=-j8 nice -n 19 emerge --onlydeps $package"
   cmd2="ln -sf /mold/mold /usr/x86_64-pc-linux-gnu/bin/ld"
-  cmd3="MAKEOPTS=-j8 FEATURES=test emerge $package"
+  cmd3="MAKEOPTS=-j8 FEATURES=test nice -n 19 emerge $package"
   filename=`echo "$package" | sed 's!/!_!g'`
   docker="docker run --rm --cap-add=SYS_PTRACE -v `pwd`:/mold mold-gentoo timeout -v -k 15s 1h"
   dir=gentoo/$git_hash
