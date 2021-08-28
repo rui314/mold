d06e2f71f31e3b3849f8ca496a90a009e53784a5
Rui Ueyama <ruiu@cs.stanford.edu>
2021-08-28 20:47:45 +0900

    Attempt to fix CI

--- test/weak-export.sh
+++ test/weak-export.sh
@@ -6,7 +6,7 @@ echo -n "Testing $(basename -s .sh $0) ... "
 t=$(pwd)/tmp/$(basename -s .sh $0)
 mkdir -p $t
 
-cat <<EOF | cc -c -o $t/a.o -xc -
+cat <<EOF | cc -fPIC -c -o $t/a.o -xc -
 #include <stdio.h>
 
 __attribute__((weak)) int foo();
