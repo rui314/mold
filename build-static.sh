76018da068c830bcdc1a3cb41a42acb0f59070d0
Rui Ueyama <ruiu@cs.stanford.edu>
2021-09-05 17:02:08 +0900

    Remove dependency to OpenSSL's RAND_bytes

--- build-static.sh
+++ build-static.sh
@@ -22,6 +22,16 @@ RUN apt-get update && \
   rm -rf /var/lib/apt/lists/*
 EOF
 
+LDFLAGS='-fuse-ld=lld -static'
+
+# libstdc++'s `std::__glibcxx_rwlock_rdlock` refers these symbols
+# as weak symbols, although they need to be defined. Otherwise,
+# the program crashes after juping to address 0.
+# So, we force loading symbols as a workaround.
+LDFLAGS="$LDFLAGS -Wl,-u,pthread_rwlock_rdlock"
+LDFLAGS="$LDFLAGS -Wl,-u,pthread_rwlock_unlock"
+LDFLAGS="$LDFLAGS -Wl,-u,pthread_rwlock_wrlock"
+
 docker run -it --rm -v `pwd`:/mold -u $(id -u):$(id -g) \
   mold-build-ubuntu20 \
-  make -C /mold -j$(nproc) EXTRA_LDFLAGS='-fuse-ld=lld -static'
+  make -C /mold -j$(nproc) EXTRA_LDFLAGS="$LDFLAGS"
