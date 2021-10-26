8471f47abb79c3220b237aabae62488f38af18a1
Rui Ueyama <rui314@gmail.com>
2021-10-26 15:24:23 +0900

    [Mach-O] wip

--- test/macho/hello.sh
+++ test/macho/hello.sh
@@ -11,13 +11,16 @@ cat <<EOF | cc -o $t/a.o -c -xc -
 void hello() {
   printf("Hello world\n");
 }
+EOF
 
+cat <<EOF | cc -o $t/b.o -c -xc -
+void hello();
 int main() {
   hello();
 }
 EOF
 
-clang -fuse-ld=$mold -o $t/exe $t/a.o
+clang -fuse-ld=$mold -o $t/exe $t/a.o $t/b.o
 $t/exe | grep -q 'Hello world'
 
 echo OK
