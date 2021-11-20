411706d1478f53a083b0a305ac1297a170daf5b9
Rui Ueyama <ruiu@cs.stanford.edu>
2021-11-20 13:03:12 +0900

    [ELF] Accept any argv[0] other than ld64 or ld64.mold
    ld is often named as x86_64-pc-linux-gnu-ld or some other system-
    dependent name. We shouldn't report an error for an unknown argv[0].

--- main.cc
+++ main.cc
@@ -41,12 +41,8 @@ void install_signal_handler() {
 int main(int argc, char **argv) {
   std::string_view cmd = mold::path_filename(argv[0]);
 
-  if (cmd == "ld" || cmd == "mold" || cmd == "ld.mold")
-    return mold::elf::main(argc, argv);
-
   if (cmd == "ld64" || cmd == "ld64.mold")
     return mold::macho::main(argc, argv);
 
-  std::cerr << "mold: unknown command: " << argv[0] << "\n";
-  exit(1);
+  return mold::elf::main(argc, argv);
 }
