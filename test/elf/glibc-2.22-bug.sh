1219d3250c8c42bef8915f516a0d6e8255691eca
Rui Ueyama <ruiu@cs.stanford.edu>
2021-11-27 21:03:31 +0900

    [ELF] Fix a test for AArch64

--- test/elf/glibc-2.22-bug.sh
+++ test/elf/glibc-2.22-bug.sh
@@ -10,14 +10,14 @@ mkdir -p $t
 # if .rela.dyn and .rela.plt are not contiguous in a given DSO.
 # This test verifies that these sections are contiguous in mold's output.
 
-cat <<EOF | cc -o $t/a.o -c -xc -
+cat <<EOF | cc -o $t/a.o -fPIC -c -xc -
 #include <stdio.h>
 int main() {
   printf("Hello world\n");
 }
 EOF
 
-clang -fuse-ld=$mold -o $t/exe $t/a.o
-readelf -W --sections $t/exe | fgrep -A1 .rela.dyn | fgrep -q .rela.plt
+clang -fuse-ld=$mold -o $t/b.so -shared $t/a.o
+readelf -W --sections $t/b.so | fgrep -A1 .rela.dyn | fgrep -q .rela.plt
 
 echo OK
