c9a2fd1d7dd3a82e295001ac3f88df7a1cf06c29
Rui Ueyama <ruiu@cs.stanford.edu>
2021-12-15 10:36:47 +0900

    Fix a test

--- test/elf/mold-wrapper2.sh
+++ test/elf/mold-wrapper2.sh
@@ -11,12 +11,6 @@ ldd $mold-wrapper.so | grep -q libasan && { echo skipped; exit; }
 rm -rf $t
 mkdir -p $t/bin $t/lib/mold
 cp $mold $t/bin
-
-$t/bin/mold -run true 2>&1 | grep -q 'mold-wrapper.so is missing'
-
-rm -rf $t
-mkdir -p $t/bin $t/lib/mold
-cp $mold $t/bin
 cp $mold-wrapper.so $t/bin
 
 $t/bin/mold -run bash -c 'echo $LD_PRELOAD' | grep -q '/bin/mold-wrapper.so'
