c824e7831ac7f03140ac30652e1e8fb41bcedd51
Rui Ueyama <ruiu@cs.stanford.edu>
2021-12-20 12:26:01 +0900

    [ELF] Show a hint if `-b binary` is given
    Fixes https://github.com/rui314/mold/issues/164

--- elf/cmdline.cc
+++ elf/cmdline.cc
@@ -681,6 +681,14 @@ void parse_nonpositional_args(Context<E> &ctx,
       }
     } else if (read_flag(args, "no-build-id")) {
       ctx.arg.build_id.kind = BuildId::NONE;
+    } else if (read_arg(ctx, args, arg, "format") ||
+               read_arg(ctx, args, arg, "b")) {
+      if (arg == "binary")
+        Fatal(ctx)
+          << "mold does not suppor `-b binary`. If you want to convert a binary"
+          " file into an object file, use `objcopy -I binary -O elf64-x86-64"
+          << " <input-file> <output-file.o>` instead.";
+      Fatal(ctx) << "mold: unknown command line option: -b " << arg;
     } else if (read_arg(ctx, args, arg, "auxiliary") ||
                read_arg(ctx, args, arg, "f")) {
       ctx.arg.auxiliary.push_back(arg);
