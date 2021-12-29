bf790b6a85a6727240c00b273fa07eb1335a20aa
Sven Johannsen <s.johannsen@bretgeld-engineering.de>
2021-12-29 13:55:19 +0100

    using PATH_MAX
    Signed-off-by: Sven Johannsen <s.johannsen@bretgeld-engineering.de>

--- elf/subprocess.cc
+++ elf/subprocess.cc
@@ -16,6 +16,7 @@
 #endif
 
 #define DAEMON_TIMEOUT 30
+constexpr size_t pathmax = PATH_MAX;
 
 namespace mold::elf {
 
@@ -251,7 +252,7 @@ void daemonize(Context<E> &ctx, std::function<void()> *wait_for_client,
 
 template <typename E>
 static std::string get_self_path(Context<E> &ctx) {
-  char buf[4096];
+  char buf[pathmax];
   size_t n = readlink("/proc/self/exe", buf, sizeof(buf));
   if (n == -1)
     Fatal(ctx) << "readlink(\"/proc/self/exe\") failed: " << errno_string();
