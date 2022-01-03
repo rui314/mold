4b2f10d0093713982b3e4636f2e3bb058065ec96
Rui Ueyama <ruiu@cs.stanford.edu>
2022-01-03 20:15:04 +0900

    [ELF] Improve test

--- test/elf/soname.sh
+++ test/elf/soname.sh
@@ -12,6 +12,10 @@ cat <<EOF | clang -fPIC -c -o "$t"/a.o -xc -
 void foo() {}
 EOF
 
+clang -o "$t"/b.so -shared "$t"/a.o
+readelf --dynamic "$t"/b.so > "$t"/log
+! fgrep -q 'Library soname' "$t"/log || false
+
 clang -fuse-ld="$mold" -o "$t"/b.so -shared "$t"/a.o -Wl,-soname,foo
 readelf --dynamic "$t"/b.so | fgrep -q 'Library soname: [foo]'
 
