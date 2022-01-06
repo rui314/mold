rui314/mold

commit 43fa021d485d6f09d74852ef30ff2ec8b48e77a5

[ELF] Do not place non-exported symbols into .gnu.hash
Previously, mold put all global symbols into .gnu.hash. Although I
believe it was not an error, it bloated the size of .gnu.hash because
.gnu.hash needs only exported symbols.

https://github.com/rui314/mold/issues/255

elf/passes.cc
@@ -509,14 +509,6 @@ void scan_rels(Context<E> &ctx) {
   // Exit if there was a relocation that refers an undefined symbol.
   ctx.checkpoint();
 
-  // Add symbol aliases for COPYREL.
-  tbb::parallel_for_each(ctx.dsos, [&](SharedFile<E> *file) {
-    for (Symbol<E> *sym : file->symbols)
-      if (sym->file == file && (sym->flags & NEEDS_COPYREL))
-        for (Symbol<E> *alias : file->find_aliases(sym))
-          alias->is_imported = true;
-  });
-
   // Aggregate dynamic symbols to a single vector.
   std::vector<InputFile<E> *> files;
   append(files, ctx.objs);
@@ -546,15 +538,19 @@ void scan_rels(Context<E> &ctx) {
       ctx.got->add_got_symbol(ctx, sym);
 
     if (sym->flags & NEEDS_PLT) {
-      if (sym->flags & NEEDS_GOT) {
+      bool is_canonical = (!ctx.arg.pic && sym->is_imported);
+
+      // If a symbol needs a canonical PLT, it is considered both
+      // imported and exported.
+      if (is_canonical)
+        sym->is_exported = true;
+
+      if ((sym->flags & NEEDS_GOT) && !is_canonical) {
+        ctx.pltgot->add_symbol(ctx, sym);
+      } else{
         // If we need to create a canonical PLT, we can't use .plt.got
         // because otherwise .plt.got and .got would refer each other,
         // resulting in an infinite loop at runtime.
-        if (!ctx.arg.pic && sym->is_imported)
-          ctx.plt->add_symbol(ctx, sym);
-        else
-          ctx.pltgot->add_symbol(ctx, sym);
-      } else {
         ctx.plt->add_symbol(ctx, sym);
       }
     }
@@ -581,7 +577,15 @@ void scan_rels(Context<E> &ctx) {
       else
         ctx.dynbss->add_symbol(ctx, sym);
 
+      // If a symbol needs copyrel, it is considered both imported
+      // and exported.
+      sym->is_exported = true;
+
+      // Aliases of this symbol are also copied so that they will be
+      // resolved to the same address at runtime.
       for (Symbol<E> *alias : file->find_aliases(sym)) {
+        alias->is_imported = true;
+        alias->is_exported = true;
         alias->has_copyrel = true;
         alias->value = sym->value;
         alias->copyrel_readonly = sym->copyrel_readonly;

elf/mold.h
@@ -621,6 +621,7 @@ public:
     this->shdr.sh_addralign = E::word_size;
   }
 
+  std::span<Symbol<E> *> get_exported_symbols(Context<E> &ctx);
   void update_shdr(Context<E> &ctx) override;
   void copy_buf(Context<E> &ctx) override;
 

elf/output-chunks.cc
@@ -950,14 +950,20 @@ template <typename E>
 void DynsymSection<E>::finalize(Context<E> &ctx) {
   Timer t(ctx, "DynsymSection::finalize");
 
-  // In any symtab, local symbols must precede global symbols.
-  auto first_global = std::stable_partition(symbols.begin() + 1, symbols.end(),
-                                            [](Symbol<E> *sym) {
+  auto is_local = [](Symbol<E> *sym) {
     return !sym->is_imported && !sym->is_exported;
+  };
+
+  // In any symtab, local symbols must precede global symbols.
+  // We also place undefined symbols before defined symbols for .gnu.hash.
+  sort(symbols.begin() + 1, symbols.end(), [&](Symbol<E> *a, Symbol<E> *b) {
+    if (auto x = (is_local(a) <=> is_local(b)); x != 0)
+      return x > 0;
+    return a->is_exported < b->is_exported;
   });
 
-  i64 global_offset = first_global - symbols.begin();
-  i64 num_globals = symbols.end() - first_global;
+  auto first_global = std::partition_point(symbols.begin() + 1, symbols.end(),
+                                           is_local);
 
   // If we have .gnu.hash section, we need to sort .dynsym contents by
   // symbol hashes.
@@ -970,11 +976,17 @@ void DynsymSection<E>::finalize(Context<E> &ctx) {
       i32 idx;
     };
 
-    std::vector<T> vec(num_globals);
-    ctx.gnu_hash->num_buckets = num_globals / ctx.gnu_hash->LOAD_FACTOR + 1;
+    auto first_exported = std::partition_point(
+      first_global, symbols.end(), [](Symbol<E> *x) { return !x->is_exported; });
 
-    tbb::parallel_for((i64)0, num_globals, [&](i64 i) {
-      Symbol<E> *sym = symbols[global_offset + i];
+    i64 base_offset = first_exported - symbols.begin();
+    i64 num_exported = symbols.end() - first_exported;
+
+    std::vector<T> vec(num_exported);
+    ctx.gnu_hash->num_buckets = num_exported / ctx.gnu_hash->LOAD_FACTOR + 1;
+
+    tbb::parallel_for((i64)0, num_exported, [&](i64 i) {
+      Symbol<E> *sym = symbols[base_offset + i];
       vec[i].sym = sym;
       vec[i].hash = djb_hash(sym->name()) % ctx.gnu_hash->num_buckets;
       vec[i].idx = i;
@@ -984,8 +996,8 @@ void DynsymSection<E>::finalize(Context<E> &ctx) {
       return std::tuple(a.hash, a.idx) < std::tuple(b.hash, b.idx);
     });
 
-    for (i64 i = 0; i < num_globals; i++)
-      symbols[global_offset + i] = vec[i].sym;
+    for (i64 i = 0; i < num_exported; i++)
+      symbols[base_offset + i] = vec[i].sym;
   }
 
   ctx.dynstr->dynsym_offset = ctx.dynstr->shdr.sh_size;
@@ -996,7 +1008,7 @@ void DynsymSection<E>::finalize(Context<E> &ctx) {
   }
 
   // ELF's symbol table sh_info holds the offset of the first global symbol.
-  this->shdr.sh_info = global_offset;
+  this->shdr.sh_info = first_global - symbols.begin();
 }
 
 template <typename E>
@@ -1089,23 +1101,34 @@ void HashSection<E>::copy_buf(Context<E> &ctx) {
 }
 
 template <typename E>
+std::span<Symbol<E> *>
+GnuHashSection<E>::get_exported_symbols(Context<E> &ctx) {
+  std::span<Symbol<E> *> syms = ctx.dynsym->symbols;
+  auto it = std::partition_point(syms.begin() + 1, syms.end(), [](Symbol<E> *sym) {
+    return !sym->is_exported;
+  });
+
+  return {it, syms.end()};
+}
+
+template <typename E>
 void GnuHashSection<E>::update_shdr(Context<E> &ctx) {
   if (ctx.dynsym->symbols.empty())
     return;
 
   this->shdr.sh_link = ctx.dynsym->shndx;
 
-  i64 num_globals = ctx.dynsym->symbols.size() - ctx.dynsym->shdr.sh_info;
-  if (num_globals) {
+  i64 num_exported = get_exported_symbols(ctx).size();
+  if (num_exported) {
     // We allocate 12 bits for each symbol in the bloom filter.
-    i64 num_bits = num_globals * 12;
+    i64 num_bits = num_exported * 12;
     num_bloom = next_power_of_two(num_bits / ELFCLASS_BITS);
   }
 
-  this->shdr.sh_size = HEADER_SIZE;              // Header
+  this->shdr.sh_size = HEADER_SIZE;               // Header
   this->shdr.sh_size += num_bloom * E::word_size; // Bloom filter
-  this->shdr.sh_size += num_buckets * 4;         // Hash buckets
-  this->shdr.sh_size += num_globals * 4;         // Hash values
+  this->shdr.sh_size += num_buckets * 4;          // Hash buckets
+  this->shdr.sh_size += num_exported * 4;         // Hash values
 }
 
 template <typename E>
@@ -1113,18 +1136,17 @@ void GnuHashSection<E>::copy_buf(Context<E> &ctx) {
   u8 *base = ctx.buf + this->shdr.sh_offset;
   memset(base, 0, this->shdr.sh_size);
 
-  i64 symoffset = ctx.dynsym->shdr.sh_info;
+  std::span<Symbol<E> *> syms = get_exported_symbols(ctx);
+  i64 exported_offset = ctx.dynsym->symbols.size() - syms.size();
+
   *(u32 *)base = num_buckets;
-  *(u32 *)(base + 4) = symoffset;
+  *(u32 *)(base + 4) = exported_offset;
   *(u32 *)(base + 8) = num_bloom;
   *(u32 *)(base + 12) = BLOOM_SHIFT;
 
-  std::span<Symbol<E> *> symbols =
-    std::span<Symbol<E> *>(ctx.dynsym->symbols).subspan(symoffset);
-
-  std::vector<u32> hashes(symbols.size());
-  for (i64 i = 0; i < symbols.size(); i++)
-    hashes[i] = djb_hash(symbols[i]->name());
+  std::vector<u32> hashes(syms.size());
+  for (i64 i = 0; i < syms.size(); i++)
+    hashes[i] = djb_hash(syms[i]->name());
 
   // Write a bloom filter
   typename E::WordTy *bloom = (typename E::WordTy *)(base + HEADER_SIZE);
@@ -1139,14 +1161,14 @@ void GnuHashSection<E>::copy_buf(Context<E> &ctx) {
   for (i64 i = 0; i < hashes.size(); i++) {
     i64 idx = hashes[i] % num_buckets;
     if (!buckets[idx])
-      buckets[idx] = i + symoffset;
+      buckets[idx] = i + exported_offset;
   }
 
   // Write a hash table
   u32 *table = buckets + num_buckets;
-  for (i64 i = 0; i < symbols.size(); i++) {
+  for (i64 i = 0; i < syms.size(); i++) {
     bool is_last = false;
-    if (i == symbols.size() - 1 ||
+    if (i == syms.size() - 1 ||
         (hashes[i] % num_buckets) != (hashes[i + 1] % num_buckets))
       is_last = true;
 

test/elf/dynamic.sh
@@ -15,7 +15,7 @@ clang -fuse-ld="$mold" -o "$t"/exe "$t"/a.o
 readelf --dynamic "$t"/exe > "$t"/log
 grep -Pq 'Shared library:.*\blibc.so\b' "$t"/log
 
-readelf -W --symbols --use-dynamic "$t"/exe > "$t"/log2
+readelf -W --dyn-syms --use-dynamic "$t"/exe > "$t"/log2
 grep -Pq 'FUNC\s+GLOBAL\s+DEFAULT\s+UND\s+__libc_start_main' "$t"/log2
 
 cat <<EOF | clang -c -fPIC -o "$t"/b.o -xc -

test/elf/gnu-hash.sh
@@ -8,14 +8,12 @@ mold="$(pwd)/mold"
 t="$(pwd)/out/test/elf/$testname"
 mkdir -p "$t"
 
-cat <<EOF | c++ -o "$t"/exe -Wl,-hash-style=gnu -xc++ -
-#include <iostream>
-
-int main() {
-  std::cout << "foo\n";
-}
+cat <<EOF | cc -c -o "$t"/a.o -xc -
+void foo() {}
+void bar() {}
+static void baz() {}
 EOF
 
-"$t"/exe | grep -q foo
+clang -fuse-ld="$mold" -o "$t"/b.so "$t"/a.o -Wl,-hash-style=gnu -shared
 
 echo OK

commit 2f86fef3a04aca180b839c8215ce57e1fde506fa

[ELF] Fix assertion failure
This patch fixes a regression introduced in
43fa021d485d6f09d74852ef30ff2ec8b48e77a5.

Fixes https://github.com/rui314/mold/issues/259

elf/passes.cc
@@ -523,13 +523,21 @@ void scan_rels(Context<E> &ctx) {
   });
 
   std::vector<Symbol<E> *> syms = flatten(vec);
+  ctx.symbol_aux.reserve(syms.size());
 
-  ctx.symbol_aux.resize(syms.size());
-  for (i64 i = 0; i < syms.size(); i++)
-    syms[i]->aux_idx = i;
+  auto add_aux = [&](Symbol<E> *sym) {
+    if (sym->aux_idx == -1) {
+      i64 sz = ctx.symbol_aux.size();
+      sym->aux_idx = sz;
+      ctx.symbol_aux.resize(sz + 1);
+    }
+  };
+
 
   // Assign offsets in additional tables for each dynamic symbol.
   for (Symbol<E> *sym : syms) {
+    add_aux(sym);
+
     if (sym->is_imported || sym->is_exported)
       ctx.dynsym->add_symbol(ctx, sym);
 
@@ -583,6 +591,7 @@ void scan_rels(Context<E> &ctx) {
       // Aliases of this symbol are also copied so that they will be
       // resolved to the same address at runtime.
       for (Symbol<E> *alias : file->find_aliases(sym)) {
+        add_aux(alias);
         alias->is_imported = true;
         alias->is_exported = true;
         alias->has_copyrel = true;

test/elf/copyrel.sh
@@ -20,15 +20,14 @@ int main() {
 }
 EOF
 
-cat <<EOF | cc -o "$t"/b.o -c -x assembler -
-  .globl foo, bar
-  .data;
-foo:
-bar:
-  .long 42
+cat <<EOF | cc -fPIC -o "$t"/b.o -c -xc -
+int foo = 42;
+extern int bar __attribute__((alias("foo")));
+extern int baz __attribute__((alias("foo")));
 EOF
 
-clang -fuse-ld="$mold" -no-pie -o "$t"/exe "$t"/a.o "$t"/b.o
+clang -fuse-ld="$mold" -shared -o "$t"/c.so "$t"/b.o
+clang -fuse-ld="$mold" -no-pie -o "$t"/exe "$t"/a.o "$t"/c.so
 "$t"/exe | grep -q '42 42 1'
 
 echo OK