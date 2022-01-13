011a7d35d6a8c58211bc7de1690d6471bbd1c5d7
Rui Ueyama <ruiu@cs.stanford.edu>
2022-01-13 13:06:01 +0900

    [ELF] Add a test
    https://github.com/rui314/mold/issues/244

---
+++ test/elf/dso-undef.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+export LANG=
+set -e
+CC="${CC:-cc}"
+CXX="${CXX:-c++}"
+testname=$(basename -s .sh "$0")
+echo -n "Testing $testname ... "
+cd "$(dirname "$0")"/../..
+mold="$(pwd)/mold"
+t=out/test/elf/$testname
+mkdir -p $t
+
+cat <<EOF | $CC -fPIC -o $t/a.o -c -xc -
+extern int foo;
+int bar = 5;
+int baz() { return foo; }
+EOF
+
+$CC -B. -o $t/b.so -shared $t/a.o
+
+cat <<EOF | $CC -o $t/c.o -c -xc -
+int foo = 3;
+EOF
+
+rm -f $t/d.a
+ar rcs $t/d.a $t/c.o
+
+cat <<EOF | $CC -o $t/e.o -c -xc -
+extern int bar;
+int main() {
+  return bar - 5;
+}
+EOF
+
+$CC -B. -o $t/exe $t/b.so $t/d.a $t/e.o
+readelf --dyn-syms $t/exe | grep -q ' foo$'
+
+echo OK
