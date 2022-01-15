12fcc76ff082a68d2c49a19dfb4fa1dac37e2835
Rui Ueyama <ruiu@cs.stanford.edu>
2022-01-15 14:02:39 +0900

    Install missing tools to a docker environment
    We need `git` and `pkg-config` because we are using them in our
    Makefile.

--- build-static.sh
+++ build-static.sh
@@ -18,7 +18,7 @@ FROM ubuntu:20.04
 RUN apt-get update && \
   TZ=Europe/London apt-get install -y tzdata && \
   apt-get install -y --no-install-recommends build-essential clang lld \
-    bsdmainutils file gcc-multilib \
+    bsdmainutils file gcc-multilib git pkg-config \
     cmake libstdc++-10-dev zlib1g-dev libssl-dev && \
   apt clean && \
   rm -rf /var/lib/apt/lists/*
