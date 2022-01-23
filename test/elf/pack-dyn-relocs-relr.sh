f8e7b157c716a8e4a25e1830ceaac999713c39ab
Rui Ueyama <ruiu@cs.stanford.edu>
2022-01-23 11:41:19 +0900

    Fix a test

--- test/elf/pack-dyn-relocs-relr.sh
+++ test/elf/pack-dyn-relocs-relr.sh
@@ -19,12 +19,17 @@ int main() {
 }
 EOF
 
-$CC -B. -o $t/exe $t/a.o -pie
-llvm-readelf -r $t/exe | grep RELATIVE | cut -d' ' -f1 | sort > $t/log1
+$CC -B. -o $t/exe1 $t/a.o -pie
+llvm-readelf -r $t/exe1 | grep RELATIVE | wc -l > $t/log1
 
-$CC -B. -o $t/exe $t/a.o -pie -Wl,-pack-dyn-relocs=relr
-llvm-readelf -r $t/exe | grep RELATIVE | cut -d' ' -f1 | sort > $t/log2
+$CC -B. -o $t/exe2 $t/a.o -pie -Wl,-pack-dyn-relocs=relr
+llvm-readelf -r $t/exe2 | grep RELATIVE | wc -l > $t/log2
 
 diff $t/log1 $t/log2
+
+llvm-readelf --dynamic $t/exe2 > $t/log3
+grep -wq RELR $t/log3
+grep -wq RELRSZ $t/log3
+grep -wq RELRENT $t/log3
 
 echo OK
