a4d9bc7550cd5569061753cc6f0d62c73848b811
Rui Ueyama <ruiu@cs.stanford.edu>
2022-02-05 12:08:24 +0900

    Fix CI

--- test/elf/warn-once.sh
+++ test/elf/warn-once.sh
@@ -10,21 +10,19 @@ mold="$(pwd)/mold"
 t=out/test/elf/$testname
 mkdir -p $t
 
-cat <<EOF | $CC -c -xc -o $t/a.o -
+cat <<EOF | $CC -c -fPIC -xc -o $t/a.o -
 extern int foo;
 int x() { return foo; }
 EOF
 
-cat <<EOF | $CC -c -xc -o $t/b.o -
+cat <<EOF | $CC -c -fPIC -xc -o $t/b.o -
 extern int foo;
 int y() { return foo; }
 int main() {}
 EOF
 
-$CC -B. -o $t/exe $t/a.o $t/b.o -Wl,--warn-unresolved-symbols >& $t/log1
-$CC -B. -o $t/exe $t/a.o $t/b.o -Wl,--warn-unresolved-symbols,--warn-once >& $t/log2
+$CC -B. -o $t/exe $t/a.o $t/b.o -Wl,--warn-unresolved-symbols,--warn-once >& $t/log
 
-[ "$(grep 'undefined symbol:.* foo$' $t/log1 | wc -l)" = 2 ]
-[ "$(grep 'undefined symbol:.* foo$' $t/log2 | wc -l)" = 1 ]
+[ "$(grep 'undefined symbol:.* foo$' $t/log | wc -l)" = 1 ]
 
 echo OK
