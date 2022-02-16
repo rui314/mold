rui314/mold

commit 60d0900496bc58dc69704789fb77fea27554de57

[ELF][LTO] Fix "concurrent hash table full" error
Since compiled IR objects may contain mergeable string sections, we
don't know the estimated number of items in output mergeable sections.
Previously, we created the concurrent hash tables for output mergeable
sections before compiling IR objects, so there was a chance to see
the "concurrent hash table full" error.

This commit moves the code to add section pieces to output mergeable
sections after `do_lto`.

elf/mold.h
@@ -1273,6 +1273,7 @@ template <typename E> void apply_exclude_libs(Context<E> &);
 template <typename E> void create_synthetic_sections(Context<E> &);
 template <typename E> void set_file_priority(Context<E> &);
 template <typename E> void resolve_symbols(Context<E> &);
+template <typename E> void register_section_pieces(Context<E> &);
 template <typename E> void eliminate_comdats(Context<E> &);
 template <typename E> void convert_common_symbols(Context<E> &);
 template <typename E> void compute_merged_section_sizes(Context<E> &);

elf/lto.cc
@@ -321,7 +321,6 @@ static PluginStatus add_input_file(const char *path) {
   file->is_alive = true;
   file->parse(ctx);
   file->resolve_symbols(ctx);
-  file->register_section_pieces(ctx);
   return LDPS_OK;
 }
 

elf/main.cc
@@ -446,13 +446,6 @@ static int elf_main(int argc, char **argv) {
     }
   }
 
-  {
-    Timer t(ctx, "register_section_pieces");
-    tbb::parallel_for_each(ctx.objs, [&](ObjectFile<E> *file) {
-      file->register_section_pieces(ctx);
-    });
-  }
-
   // Uniquify shared object files by soname
   {
     std::unordered_set<std::string_view> seen;
@@ -478,6 +471,9 @@ static int elf_main(int argc, char **argv) {
   // Do LTO
   if (ctx.has_lto_object)
     do_lto(ctx);
+
+  // Resolve mergeable section pieces to merge them.
+  register_section_pieces(ctx);
 
   // Remove redundant comdat sections (e.g. duplicate inline functions).
   eliminate_comdats(ctx);

elf/passes.cc
@@ -130,6 +130,15 @@ void resolve_symbols(Context<E> &ctx) {
 }
 
 template <typename E>
+void register_section_pieces(Context<E> &ctx) {
+  Timer t(ctx, "register_section_pieces");
+
+  tbb::parallel_for_each(ctx.objs, [&](ObjectFile<E> *file) {
+    file->register_section_pieces(ctx);
+  });
+}
+
+template <typename E>
 void eliminate_comdats(Context<E> &ctx) {
   Timer t(ctx, "eliminate_comdats");
 
@@ -1320,6 +1329,7 @@ void compress_debug_sections(Context<E> &ctx) {
   template void apply_exclude_libs(Context<E> &);                       \
   template void create_synthetic_sections(Context<E> &);                \
   template void resolve_symbols(Context<E> &);                          \
+  template void register_section_pieces(Context<E> &);                  \
   template void eliminate_comdats(Context<E> &);                        \
   template void convert_common_symbols(Context<E> &);                   \
   template void compute_merged_section_sizes(Context<E> &);             \

commit d908d4c61c2b67a7064a21c78285d528b1f62b5a

[ELF] Handle global symbols in mergeable sections correctly
Prior to this patch, mold computeed a wrong address for a global
symbol in a mergeable section. believe it was a regression in
60d0900496bc58dc69704789fb77fea27554de57.

Fixes https://github.com/rui314/mold/issues/347

elf/input-files.cc
@@ -816,12 +816,7 @@ void ObjectFile<E>::resolve_symbols(Context<E> &ctx) {
     if (get_rank(this, esym, !this->is_alive) < get_rank(sym)) {
       sym.file = this;
       sym.input_section = isec;
-
-      if (SectionFragmentRef<E> &ref = sym_fragments[i]; ref.frag)
-        sym.value = ref.addend;
-      else
-        sym.value = esym.st_value;
-
+      sym.value = esym.st_value;
       sym.sym_idx = i;
       sym.ver_idx = ctx.default_version;
       sym.is_weak = esym.is_weak();

elf/mold.h
@@ -2030,16 +2030,20 @@ inline bool Symbol<E>::operator<(const Symbol &other) const {
 
 template <typename E>
 inline u64 Symbol<E>::get_addr(Context<E> &ctx, bool allow_plt) const {
-  if (SectionFragment<E> *frag = get_frag()) {
-    if (!frag->is_alive) {
-      // This condition is met if a non-alloc section refers an
-      // alloc section and if the referenced piece of data is
-      // garbage-collected. Typically, this condition is met if a
-      // debug info section referring a string constant in .rodata.
-      return 0;
-    }
+  if (file && !file->is_dso) {
+    SectionFragmentRef<E> &ref = ((ObjectFile<E> *)file)->sym_fragments[sym_idx];
 
-    return frag->get_addr(ctx) + value;
+    if (ref.frag) {
+      if (!ref.frag->is_alive) {
+        // This condition is met if a non-alloc section refers an
+        // alloc section and if the referenced piece of data is
+        // garbage-collected. Typically, this condition is met if a
+        // debug info section referring a string constant in .rodata.
+        return 0;
+      }
+
+      return ref.frag->get_addr(ctx) + ref.addend;
+    }
   }
 
   if (has_copyrel) {

test/elf/mergeable-records.sh
@@ -1,0 +1,65 @@
+#!/bin/bash
+export LANG=
+set -e
+CC="${CC:-cc}"
+CXX="${CXX:-c++}"
+testname=$(basename "$0" .sh)
+echo -n "Testing $testname ... "
+cd "$(dirname "$0")"/../..
+mold="$(pwd)/mold"
+t=out/test/elf/$testname
+mkdir -p $t
+
+# Skip if target is not x86-64
+[ "$(uname -m)" = x86_64 ] || { echo skipped; exit; }
+
+cat <<'EOF' | $CC -o $t/a.o -c -x assembler -
+  .text
+  .globl main
+main:
+  sub $8, %rsp
+
+  mov $.L.str1, %rdi
+  xor %rax, %rax
+  call printf
+
+  mov $.L.str1+1, %rdi
+  xor %rax, %rax
+  call printf
+
+  mov $str2+2, %rdi
+  xor %rax, %rax
+  call printf
+
+  mov $.L.str3+3, %rdi
+  xor %rax, %rax
+  call printf
+
+  mov $.rodata.cst8+16, %rdi
+  xor %rax, %rax
+  call printf
+
+  xor %rax, %rax
+  add $8, %rsp
+  ret
+
+  .section .rodata.cst8, "aM", @progbits, 8
+  .align 8
+.L.str1:
+  .ascii "abcdef\n\0"
+.globl str2
+str2:
+  .ascii "ghijkl\n\0"
+.L.str3:
+  .ascii "mnopqr\n\0"
+EOF
+
+$CC -B. -static -o $t/exe $t/a.o
+
+$t/exe | grep -q '^abcdef$'
+$t/exe | grep -q '^bcdef$'
+$t/exe | grep -q '^ijkl$'
+$t/exe | grep -q '^pqr$'
+$t/exe | grep -q '^mnopqr$'
+
+echo OK