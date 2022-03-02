99a0aaf13325679f4dc13538ac4ea26bc8343213
Rui Ueyama <ruiu@cs.stanford.edu>
2022-03-02 10:35:30 +0900

    [ELF] Add a test for -init

---
+++ test/elf/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export LANG=
+set -e
+CC="${CC:-cc}"
+CXX="${CXX:-c++}"
+testname=$(basename "$0" .sh)
+echo -n "Testing $testname ... "
+cd "$(dirname "$0")"/../..
+mold="$(pwd)/mold"
+t=out/test/elf/$testname
+mkdir -p $t
+
+cat <<EOF | $CC -o $t/a.o -c -xc -
+void foo() {}
+int main() {}
+EOF
+
+$CC -B. -o $t/exe $t/a.o -Wl,-init,foo
+readelf --dynamic $t/exe | fgrep -q '(INIT)'
+
+$CC -B. -o $t/exe $t/a.o -Wl,-init,no-such-symbol
+readelf --dynamic $t/exe > $t/log
+! fgrep -q '(INIT)' $t/log || false
+
+echo OK
