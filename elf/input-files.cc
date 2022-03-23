812c59fd5c4a4577ef7d8ed9f809490df5f80701
Rui Ueyama <ruiu@cs.stanford.edu>
2022-03-23 15:00:29 +0000

    Refactor

--- elf/input-files.cc
+++ elf/input-files.cc
@@ -156,11 +156,9 @@ void ObjectFile<E>::initialize_sections(Context<E> &ctx) {
     case SHT_REL:
     case SHT_RELA:
     case SHT_NULL:
+    case SHT_ARM_ATTRIBUTES:
       break;
     default: {
-      if (E::e_machine == EM_ARM && shdr.sh_type == SHT_ARM_ATTRIBUTES)
-        continue;
-
       std::string_view name = this->shstrtab.data() + shdr.sh_name;
       if (name == ".note.GNU-stack" || name.starts_with(".gnu.warning."))
         continue;
