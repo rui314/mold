face536c391e1b61a315deb0ab48f0001555d06b
Luboš Luňák <l.lunak@centrum.cz>
2022-04-26 09:12:29 +0200

    launch gdb with -nx (ignore all .gdbinit)
    Otherwise the test may fail simply because .gdbinit setup does
    something strange (I happen to have 'set auto-solib-add off', which
    disables loading debug info by default).

--- test/elf/gdb-index-compress-output.sh
+++ test/elf/gdb-index-compress-output.sh
@@ -49,7 +49,7 @@ readelf -WS $t/exe 2> /dev/null | fgrep -q .gdb_index
 
 $QEMU $t/exe | grep -q 'Hello world'
 
-DEBUGINFOD_URLS= gdb $t/exe -batch -ex 'b main' -ex r -ex 'b trap' \
+DEBUGINFOD_URLS= gdb $t/exe -nx -batch -ex 'b main' -ex r -ex 'b trap' \
   -ex c -ex bt -ex quit >& $t/log
 
 grep -Pq 'hello \(\) at .*<stdin>:7' $t/log
