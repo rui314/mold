rui314/mold

commit 7ebd071273691a5cc095203f6542b2b13ec6642c

[ELF] Recognize --thinlto-{single-module,cache-dir,cache-policy}
They are defined as aliases for
--plugin-opt=thinlto-{single-module,cache-dir,cache-policy}.

https://github.com/rui314/mold/issues/336

elf/cmdline.cc
@@ -868,6 +868,12 @@ void parse_nonpositional_args(Context<E> &ctx,
       ctx.arg.plugin_opt.push_back("thinlto-object-suffix-replace=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-prefix-replace")) {
       ctx.arg.plugin_opt.push_back("thinlto-prefix-replace=" + std::string(arg));
+    } else if (read_arg(ctx, args, arg, "thinlto-single-module")) {
+      ctx.arg.plugin_opt.push_back("thinlto-single-module=" + std::string(arg));
+    } else if (read_arg(ctx, args, arg, "thinlto-cache-dir")) {
+      ctx.arg.plugin_opt.push_back("thinlto-cache-dir=" + std::string(arg));
+    } else if (read_arg(ctx, args, arg, "thinlto-cache-policy")) {
+      ctx.arg.plugin_opt.push_back("thinlto-cache-policy=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-jobs")) {
       ctx.arg.plugin_opt.push_back("jobs=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thread-count")) {

commit 1bd48be605ed1ea824bed11131c70c70c5151a46

[ELF] Fix LLVMgold ThinLTO cache option names
These options needs to be passed without the thinlto- prefix.

See also: https://github.com/llvm/llvm-project/blob/6365bde6585651b7813010dc63df7984a5b8ad41/llvm/tools/gold/gold-plugin.cpp#L263-L266

Fixes: 7ebd0712 ("[ELF] Recognize --thinlto-{single-module,cache-dir,cache-policy}")
Signed-off-by: Tatsuyuki Ishi <ishitatsuyuki@gmail.com>

elf/cmdline.cc
@@ -874,9 +874,9 @@ void parse_nonpositional_args(Context<E> &ctx,
     } else if (read_arg(ctx, args, arg, "thinlto-single-module")) {
       ctx.arg.plugin_opt.push_back("thinlto-single-module=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-cache-dir")) {
-      ctx.arg.plugin_opt.push_back("thinlto-cache-dir=" + std::string(arg));
+      ctx.arg.plugin_opt.push_back("cache-dir=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-cache-policy")) {
-      ctx.arg.plugin_opt.push_back("thinlto-cache-policy=" + std::string(arg));
+      ctx.arg.plugin_opt.push_back("cache-policy=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-jobs")) {
       ctx.arg.plugin_opt.push_back("jobs=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thread-count")) {