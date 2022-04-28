rui314/mold

commit 7ebd071273691a5cc095203f6542b2b13ec6642c

[ELF] Recognize --thinlto-{single-module,cache-dir,cache-policy}
They are defined as aliases for
--plugin-opt=thinlto-{single-module,cache-dir,cache-policy}.

https://github.com/rui314/mold/issues/336

elf/cmdline.cc
@@ -868,6 +868,12 @@ void parse_nonpositional_args(Context<E> &ctx,
       ctx.arg.plugin_opt.push_back("thinlto-object-suffix-replace=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-prefix-replace")) {
       ctx.arg.plugin_opt.push_back("thinlto-prefix-replace=" + std::string(arg));
+    } else if (read_arg(ctx, args, arg, "thinlto-single-module")) {
+      ctx.arg.plugin_opt.push_back("thinlto-single-module=" + std::string(arg));
+    } else if (read_arg(ctx, args, arg, "thinlto-cache-dir")) {
+      ctx.arg.plugin_opt.push_back("thinlto-cache-dir=" + std::string(arg));
+    } else if (read_arg(ctx, args, arg, "thinlto-cache-policy")) {
+      ctx.arg.plugin_opt.push_back("thinlto-cache-policy=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-jobs")) {
       ctx.arg.plugin_opt.push_back("jobs=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thread-count")) {

commit 3b9dc732106e9c970a1eea6e2e1e4a960d2950b1

[ELF] Remove support for thinlto-single-module
This is a LLD exclusive option that is not supported by LLVMgold. It
will be broken, so remove it for now.

Fixes: 7ebd0712 ("[ELF] Recognize --thinlto-{single-module,cache-dir,cache-policy}")
Signed-off-by: Tatsuyuki Ishi <ishitatsuyuki@gmail.com>

elf/cmdline.cc
@@ -871,8 +871,6 @@ void parse_nonpositional_args(Context<E> &ctx,
       ctx.arg.plugin_opt.push_back("thinlto-object-suffix-replace=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-prefix-replace")) {
       ctx.arg.plugin_opt.push_back("thinlto-prefix-replace=" + std::string(arg));
-    } else if (read_arg(ctx, args, arg, "thinlto-single-module")) {
-      ctx.arg.plugin_opt.push_back("thinlto-single-module=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-cache-dir")) {
       ctx.arg.plugin_opt.push_back("cache-dir=" + std::string(arg));
     } else if (read_arg(ctx, args, arg, "thinlto-cache-policy")) {