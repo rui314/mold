a6c3f3c08267713e3f8d649189e3128a8042d4e7
Rui Ueyama <ruiu@bluewhale.systems>
2022-05-14 14:02:11 +0800

    Add more dependencies

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -1,23 +1,30 @@
 #!/bin/bash -x
 set -e
-
 source /etc/os-release
+
+# The first line for each distro installs a build dependency.
+# The second line installs extra packages for `make test`.
 
 case "$ID$VERSION_ID" in
 ubuntu20.04)
-  apt-get install -y build-essential git g++-10 cmake libssl-dev zlib1g-dev git
+  apt-get install -y build-essential git cmake libssl-dev zlib1g-dev git g++-10
+  apt-get install -y file bsdmainutils
   ;;
 ubuntu22.04)
-  apt-get install -y build-essential git g++ cmake libssl-dev zlib1g-dev git
+  apt-get install -y build-essential git cmake libssl-dev zlib1g-dev git g++
+  apt-get install -y file bsdmainutils
   ;;
 fedora*)
   dnf install -y git gcc-g++ cmake openssl-devel zlib-devel
+  dnf install -y glibc-static file libstdc++-static diffutils
   ;;
 opensuse-leap*)
-  zypper install -y git make cmake zlib-devel libopenssl-devel gcc11-++c
+  zypper install -y git make cmake zlib-devel libopenssl-devel gcc11-c++
+  zypper install -y glibc-devel-static tar diffutils util-linux
   ;;
 opensuse-tumbleweed*)
   zypper install -y git make cmake zlib-devel libopenssl-devel gcc-c++
+  zypper install -y glibc-devel-static tar diffutils util-linux
   ;;
 gentoo*)
   emerge dev-vcs/git dev-util/cmake sys-libs/zlib
