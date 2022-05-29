395b962c3d247a40c06965260fd4c50e2bf958c9
Rui Ueyama <ruiu@bluewhale.systems>
2022-05-29 15:09:30 +0800

    [Mach-O] Use SHA1 instead of SHA256 for code signature
    It looks like SHA1 is slightly faster than SHA256 on a 2021 ARM Mac mini.
    SHA1 is not recommended anymore, but for ad-hoc code signing, it should
    be enough because anyone can just recompute an ad-hoc code signature for
    any arbitrary binary.

--- sha.h
+++ sha.h
@@ -1,6 +1,7 @@
 #ifdef __APPLE__
 #  define COMMON_DIGEST_FOR_OPENSSL
 #  include <CommonCrypto/CommonDigest.h>
+#  define SHA1(data, len, md) CC_SHA1(data, len, md)
 #  define SHA256(data, len, md) CC_SHA256(data, len, md)
 #else
 #  define OPENSSL_SUPPRESS_DEPRECATED 1
