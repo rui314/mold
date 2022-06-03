0723476fa5c68395794dae271e9b1032508c6f24
Rui Ueyama <ruiu@bluewhale.systems>
2022-06-03 11:50:05 +0800

    Add a test

---
+++ test/macho/private-extern.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export LC_ALL=C
+set -e
+CC="${TEST_CC:-cc}"
+CXX="${TEST_CXX:-c++}"
+GCC="${TEST_GCC:-gcc}"
+GXX="${TEST_GXX:-g++}"
+OBJDUMP="${OBJDUMP:-objdump}"
+MACHINE="${MACHINE:-$(uname -m)}"
+testname=$(basename "$0" .sh)
+echo -n "Testing $testname ... "
+cd "$(dirname "$0")"/../..
+t=out/test/macho/$testname
+mkdir -p $t
+
+cat <<EOF | $CC -o $t/a.o -c -xc -
+void foo() {}
+__attribute__((visibility("hidden"))) void bar() {}
+EOF
+
+clang --ld-path=./ld64 -shared -o $t/b.dylib $t/a.o
+objdump --macho --exports-trie $t/b.dylib > $t/log
+grep -q _foo $t/log
+! grep -q _bar $t/log || false
+
+echo OK
