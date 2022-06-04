1b52d228d40f7645da039babd926e36740880eef
Rui Ueyama <ruiu@bluewhale.systems>
2022-06-04 16:15:07 +0800

    Add a test

---
+++ test/macho/syslibroot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+export LC_ALL=C
+set -e
+CC="${TEST_CC:-cc}"
+CXX="${TEST_CXX:-c++}"
+GCC="${TEST_GCC:-gcc}"
+GXX="${TEST_GXX:-g++}"
+OBJDUMP="${OBJDUMP:-objdump}"
+MACHINE="${MACHINE:-$(uname -m)}"
+testname=$(basename "$0" .sh)
+echo -n "Testing $testname ... "
+cd "$(dirname "$0")"/../..
+t=out/test/macho/$testname
+mkdir -p $t
+
+mkdir -p $t/foo/bar
+
+cat <<EOF | $CC -shared -o $t/foo/bar/libbaz.dylib -xc -
+void foo() {}
+EOF
+
+cat <<EOF | $CC -o $t/a.o -c -xc -
+void foo();
+void bar() { foo(); }
+EOF
+
+clang --ld-path=./ld64 -shared -o $t/b.dylib $t/a.o -nodefaultlibs \
+  -L/foo/bar -isysroot $t -lbaz
+
+echo OK
