96d9859f4597f015c74b996b175cf511bd7b39fa
Rui Ueyama <ruiu@bluewhale.systems>
2022-06-04 19:25:28 +0800

    [Mach-O] Add a workaround for linking Chrome

--- macho/main.cc
+++ macho/main.cc
@@ -254,6 +254,12 @@ static void claim_unresolved_symbols(Context<E> &ctx) {
     if (Symbol<E> *sym = get_symbol(ctx, name); !sym->file)
       sym->is_imported = true;
 
+  // This is a workaround to link Chromium.
+  if (Symbol<E> *sym = get_symbol(ctx, "_OBJC_CLASS_$_NSThread"); !sym->file)
+    sym->is_imported = true;
+  if (Symbol<E> *sym = get_symbol(ctx, "_OBJC_CLASS_$_NSFont"); !sym->file)
+    sym->is_imported = true;
+
   for (ObjectFile<E> *file : ctx.objs) {
     for (i64 i = 0; i < file->mach_syms.size(); i++) {
       MachSym &msym = file->mach_syms[i];
