0e88fd047e01a32aba268d39841c3daf3c38baf7
Rui Ueyama <ruiu@bluewhale.systems>
2022-07-04 19:22:05 +0800

    Add `extern "C"`

--- third-party/rust-demangle/rust-demangle.h
+++ third-party/rust-demangle/rust-demangle.h
@@ -3,8 +3,16 @@
 
 #define RUST_DEMANGLE_FLAG_VERBOSE 1
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool rust_demangle_with_callback(
     const char *mangled, int flags,
     void (*callback)(const char *data, size_t len, void *opaque), void *opaque
 );
 char *rust_demangle(const char *mangled, int flags);
+
+#ifdef __cplusplus
+}
+#endif
