5071c42e727d5883a6eb123f0664ac8d57fae9a1
Rui Ueyama <ruiu@bluewhale.systems>
2022-07-05 15:01:13 +0800

    Add a test for Rust demangler
    https://github.com/rui314/mold/issues/371

---
+++ test/elf/demangle-rust.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export LC_ALL=C
+set -e
+CC="${TEST_CC:-cc}"
+CXX="${TEST_CXX:-c++}"
+GCC="${TEST_GCC:-gcc}"
+GXX="${TEST_GXX:-g++}"
+OBJDUMP="${OBJDUMP:-objdump}"
+MACHINE="${MACHINE:-$(uname -m)}"
+testname=$(basename "$0" .sh)
+echo -n "Testing $testname ... "
+cd "$(dirname "$0")"/../..
+t=out/test/elf/$testname
+mkdir -p $t
+
+[ $MACHINE = x86_64 ] || { echo skipped; exit; }
+
+cat <<'EOF' | $CC -c -o $t/a.o -xassembler -
+.globl main
+main:
+call _RNCINkXs25_NgCsbmNqQUJIY6D_4core5sliceINyB9_4IterhENuNgNoBb_4iter8iterator8Iterator9rpositionNCNgNpB9_6memchr7memrchrs_0E0Bb_
+EOF
+
+! $CC -B. -o $t/exe $t/a.o 2> $t/log || false
+fgrep -q '<core::slice::Iter<u8> as core::iter::iterator::Iterator>::rposition::<core::slice::memchr::memrchr::{closure#1}>::{closure#0}' $t/log
+
+echo OK
