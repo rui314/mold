58f393ac8b903125e3f4f19cfc60274bba906cb0
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2022-07-20 11:42:19 +0900

    [elf] Add test for --export-dynamic-symbol[-list].
    Piggybacked on existing tests for --dynamic-list.
    
    Signed-off-by: Tatsuyuki Ishi <ishitatsuyuki@gmail.com>

--- test/elf/dynamic-list2.sh
+++ test/elf/dynamic-list2.sh
@@ -32,11 +32,18 @@ cat <<EOF > $t/dyn
 { foo; extern "C++" { "baz(int)"; }; };
 EOF
 
-$CC -B. -o $t/exe $t/a.o $t/b.o -Wl,-dynamic-list=$t/dyn
+$CC -B. -o $t/exe1 $t/a.o $t/b.o -Wl,-dynamic-list=$t/dyn
 
-readelf --dyn-syms $t/exe > $t/log
-grep -q ' foo$' $t/log
-! grep -q ' bar$' $t/log || false
-grep -q ' _Z3bazi$' $t/log
+readelf --dyn-syms $t/exe1 > $t/log1
+grep -q ' foo$' $t/log1
+! grep -q ' bar$' $t/log1 || false
+grep -q ' _Z3bazi$' $t/log1
+
+$CC -B. -o $t/exe2 $t/a.o $t/b.o -Wl,--export-dynamic-symbol-list=$t/dyn
+
+readelf --dyn-syms $t/exe2 > $t/log2
+grep -q ' foo$' $t/log2
+! grep -q ' bar$' $t/log2 || false
+grep -q ' _Z3bazi$' $t/log2
 
 echo OK
