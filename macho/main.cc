13ef968fb113c8a14e003a2e36eae15527fde17a
Rui Ueyama <ruiu@bluewhale.systems>
2022-07-30 14:49:41 +0800

    [Mach-O] Do not read the same library or framework more than once

--- macho/main.cc
+++ macho/main.cc
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <tbb/concurrent_vector.h>
 #include <tbb/global_control.h>
@@ -657,51 +658,6 @@ static void compute_uuid(Context<E> &ctx) {
 }
 
 template <typename E>
-MappedFile<Context<E>> *find_framework(Context<E> &ctx, std::string name) {
-  std::string suffix;
-  std::tie(name, suffix) = split_string(name, ',');
-
-  for (std::string path : ctx.arg.framework_paths) {
-    path = get_realpath(path + "/" + name + ".framework/" + name);
-
-    if (!suffix.empty())
-      if (auto *mf = MappedFile<Context<E>>::open(ctx, path + suffix))
-        return mf;
-
-    if (auto *mf = MappedFile<Context<E>>::open(ctx, path + ".tbd"))
-      return mf;
-
-    if (auto *mf = MappedFile<Context<E>>::open(ctx, path))
-      return mf;
-  }
-  Fatal(ctx) << "-framework not found: " << name;
-}
-
-template <typename E>
-MappedFile<Context<E>> *find_library(Context<E> &ctx, std::string name) {
-  auto search = [&](std::vector<std::string> extn) -> MappedFile<Context<E>> * {
-    for (std::string dir : ctx.arg.library_paths) {
-      for (std::string e : extn) {
-        std::string path = dir + "/lib" + name + e;
-        if (MappedFile<Context<E>> *mf = MappedFile<Context<E>>::open(ctx, path))
-          return mf;
-        ctx.missing_files.insert(path);
-      }
-    }
-    return nullptr;
-  };
-
-  // -search_paths_first
-  if (ctx.arg.search_paths_first)
-    return search({".tbd", ".dylib", ".a"});
-
-  // -search_dylibs_first
-  if (MappedFile<Context<E>> *mf = search({".tbd", ".dylib"}))
-    return mf;
-  return search({".a"});
-}
-
-template <typename E>
 static MappedFile<Context<E>> *
 strip_universal_header(Context<E> &ctx, MappedFile<Context<E>> *mf) {
   FatHeader &hdr = *(FatHeader *)mf->data;
@@ -723,20 +679,13 @@ static void read_file(Context<E> &ctx, MappedFile<Context<E>> *mf) {
   case FileType::TAPI:
   case FileType::MACH_DYLIB:
   case FileType::MACH_EXE:
-    if (ctx.loaded_files.insert(mf->name).second)
-      ctx.dylibs.push_back(DylibFile<E>::create(ctx, mf));
+    ctx.dylibs.push_back(DylibFile<E>::create(ctx, mf));
     break;
   case FileType::MACH_OBJ:
   case FileType::LLVM_BITCODE:
     ctx.objs.push_back(ObjectFile<E>::create(ctx, mf, ""));
     break;
   case FileType::AR:
-    if (!ctx.all_load && !ctx.loaded_files.insert(mf->name).second) {
-      // If the same .a file is specified more than once, ignore all
-      // but the first one because they would be ignored anyway.
-      break;
-    }
-
     for (MappedFile<Context<E>> *child : read_archive_members(ctx, mf))
       if (get_file_type(child) == FileType::MACH_OBJ)
         ctx.objs.push_back(ObjectFile<E>::create(ctx, child, mf->name));
@@ -785,11 +734,71 @@ template <typename E>
 static void read_input_files(Context<E> &ctx, std::span<std::string> args) {
   Timer t(ctx, "read_input_files");
 
-  auto must_find_library = [&](std::string arg) {
-    MappedFile<Context<E>> *mf = find_library(ctx, arg);
+  std::unordered_set<std::string> libs;
+  std::unordered_set<std::string> frameworks;
+
+  auto search = [&](std::vector<std::string> names)
+      -> MappedFile<Context<E>> * {
+    for (std::string dir : ctx.arg.library_paths) {
+      for (std::string name : names) {
+        std::string path = dir + "/lib" + name;
+        if (MappedFile<Context<E>> *mf = MappedFile<Context<E>>::open(ctx, path))
+          return mf;
+        ctx.missing_files.insert(path);
+      }
+    }
+    return nullptr;
+  };
+
+  auto find_library = [&](std::string name) -> MappedFile<Context<E>> * {
+    // -search_paths_first
+    if (ctx.arg.search_paths_first)
+      return search({name + ".tbd", name + ".dylib", name + ".a"});
+
+    // -search_dylibs_first
+    if (MappedFile<Context<E>> *mf = search({name + ".tbd", name + ".dylib"}))
+      return mf;
+    return search({name + ".a"});
+  };
+
+  auto read_library = [&](std::string name) {
+    if (!libs.insert(name).second)
+      return;
+
+    MappedFile<Context<E>> *mf = find_library(name);
     if (!mf)
-      Fatal(ctx) << "library not found: -l" << arg;
-    return mf;
+      Fatal(ctx) << "library not found: -l" << name;
+    read_file(ctx, mf);
+  };
+
+  auto find_framework = [&](std::string name) -> MappedFile<Context<E>> * {
+    std::string suffix;
+    std::tie(name, suffix) = split_string(name, ',');
+
+    for (std::string path : ctx.arg.framework_paths) {
+      path = get_realpath(path + "/" + name + ".framework/" + name);
+
+      if (!suffix.empty())
+        if (auto *mf = MappedFile<Context<E>>::open(ctx, path + suffix))
+          return mf;
+
+      if (auto *mf = MappedFile<Context<E>>::open(ctx, path + ".tbd"))
+        return mf;
+
+      if (auto *mf = MappedFile<Context<E>>::open(ctx, path))
+        return mf;
+    }
+    return nullptr;
+  };
+
+  auto read_framework = [&](std::string name) {
+    if (!frameworks.insert(name).second)
+      return;
+
+    MappedFile<Context<E>> *mf = find_framework(name);
+    if (!mf)
+      Fatal(ctx) << "-framework not found: " << name;
+    read_file(ctx, mf);
   };
 
   while (!args.empty()) {
@@ -830,27 +839,27 @@ static void read_input_files(Context<E> &ctx, std::span<std::string> args) {
       read_file(ctx, MappedFile<Context<E>>::must_open(ctx, arg));
       ctx.all_load = orig;
     } else if (opt == "-framework") {
-      read_file(ctx, find_framework(ctx, arg));
+      read_framework(arg);
     } else if (opt == "-needed_framework") {
       ctx.needed_l = true;
-      read_file(ctx, find_framework(ctx, arg));
+      read_framework(arg);
     } else if (opt == "-weak_framework") {
       ctx.weak_l = true;
-      read_file(ctx, find_framework(ctx, arg));
+      read_framework(arg);
     } else if (opt == "-l") {
-      read_file(ctx, must_find_library(arg));
+      read_library(arg);
     } else if (opt == "-needed-l") {
       ctx.needed_l = true;
-      read_file(ctx, must_find_library(arg));
+      read_library(arg);
     } else if (opt == "-hidden-l") {
       ctx.hidden_l = true;
-      read_file(ctx, must_find_library(arg));
+      read_library(arg);
     } else if (opt == "-weak-l") {
       ctx.weak_l = true;
-      read_file(ctx, must_find_library(arg));
+      read_library(arg);
     } else if (opt == "-reexport-l") {
       ctx.reexport_l = true;
-      read_file(ctx, must_find_library(arg));
+      read_library(arg);
     } else {
       unreachable();
     }
@@ -863,7 +872,7 @@ static void read_input_files(Context<E> &ctx, std::span<std::string> args) {
 
   // With -bundle_loader, we can import symbols from a main executable.
   if (!ctx.arg.bundle_loader.empty())
-    read_file(ctx, MappedFile<Context<E>>::must_open(ctx, ctx.arg.bundle_loader));
+    read_library(ctx.arg.bundle_loader);
 
   // An object file can contain linker directives to load other object
   // files or libraries, so process them if any.
@@ -873,11 +882,13 @@ static void read_input_files(Context<E> &ctx, std::span<std::string> args) {
 
     for (i64 j = 0; j < opts.size();) {
       if (opts[j] == "-framework") {
-        read_file(ctx, find_framework(ctx, opts[j + 1]));
+        read_framework(opts[j + 1]);
         j += 2;
       } else if (opts[j].starts_with("-l")) {
-        if (MappedFile<Context<E>> *mf = find_library(ctx, opts[j].substr(2)))
-          read_file(ctx, mf);
+        std::string name = opts[j].substr(2);
+        if (libs.insert(name).second)
+          if (MappedFile<Context<E>> *mf = find_library(name))
+            read_file(ctx, mf);
         j++;
       } else {
         Fatal(ctx) << *file << ": unknown LC_LINKER_OPTION command: " << opts[j];
