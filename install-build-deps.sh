b9d03e09c0d13e9dd80d2668add9ef05e467cc3a
Nicolas Barrios <nbarrios1337@gmail.com>
2022-08-05 01:23:19 -0400

    Support derivative OSes when installing deps
    Signed-off-by: Nicolas Barrios <nbarrios1337@gmail.com>

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -4,35 +4,45 @@ source /etc/os-release
 
 # The first line for each distro installs a build dependency.
 # The second line installs extra packages for `make test`.
+test_id_version() {
+  case "$1$2" in
+  ubuntu20.*)
+    apt-get install -y git cmake libssl-dev zlib1g-dev gcc g++ g++-10
+    apt-get install -y file bsdmainutils
+    ;;
+  ubuntu22.* | debian11)
+    apt-get install -y git cmake libssl-dev zlib1g-dev gcc g++
+    apt-get install -y file bsdmainutils
+    ;;
+  fedora*)
+    dnf install -y git gcc-g++ cmake openssl-devel zlib-devel
+    dnf install -y glibc-static file libstdc++-static diffutils
+    ;;
+  opensuse-leap*)
+    zypper install -y git make cmake zlib-devel libopenssl-devel gcc-c++ gcc11-c++
+    zypper install -y glibc-devel-static tar diffutils util-linux
+    ;;
+  opensuse-tumbleweed*)
+    zypper install -y git make cmake zlib-devel libopenssl-devel gcc-c++
+    zypper install -y glibc-devel-static tar diffutils util-linux
+    ;;
+  gentoo*)
+    emerge dev-vcs/git dev-util/cmake sys-libs/zlib
+    ;;
+  arch*)
+    pacman -S --needed --noconfirm base-devel zlib openssl cmake util-linux git
+    ;;
+  *)
+    return 1
+    ;;
+  esac
+}
 
-case "$ID$VERSION_ID" in
-ubuntu20.*)
-  apt-get install -y git cmake libssl-dev zlib1g-dev gcc g++ g++-10
-  apt-get install -y file bsdmainutils
-  ;;
-ubuntu22.* | debian11)
-  apt-get install -y git cmake libssl-dev zlib1g-dev gcc g++
-  apt-get install -y file bsdmainutils
-  ;;
-fedora*)
-  dnf install -y git gcc-g++ cmake openssl-devel zlib-devel
-  dnf install -y glibc-static file libstdc++-static diffutils
-  ;;
-opensuse-leap*)
-  zypper install -y git make cmake zlib-devel libopenssl-devel gcc-c++ gcc11-c++
-  zypper install -y glibc-devel-static tar diffutils util-linux
-  ;;
-opensuse-tumbleweed*)
-  zypper install -y git make cmake zlib-devel libopenssl-devel gcc-c++
-  zypper install -y glibc-devel-static tar diffutils util-linux
-  ;;
-gentoo*)
-  emerge dev-vcs/git dev-util/cmake sys-libs/zlib
-  ;;
-arch*)
-  pacman -S --needed --noconfirm base-devel zlib openssl cmake util-linux git
-  ;;
-*)
+if ! test_id_version "$ID" "$VERSION_ID" ; then
   echo "Error: don't know anything about build dependencies on $ID $VERSION_ID"
-  exit 1
-esac
+  echo "Testing related OS"
+  if ! test_id_version "${ID_LIKE%% *}" "$VERSION_ID" ; then
+    echo "Error: don't know anything about build dependencies on ${ID_LIKE%% *} $VERSION_ID"
+    exit 1
+  fi
+fi
