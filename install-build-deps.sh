627cb0664c9bbd6acddc1f313ded8a03808d99c4
Rui Ueyama <ruiu@bluewhale.systems>
2022-08-05 14:40:06 +0800

    Avoid possible accidental prefix match

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -5,31 +5,31 @@ source /etc/os-release
 # The first line for each distro installs a build dependency.
 # The second line installs extra packages for `make test`.
 
-case "$ID$VERSION_ID" in
-ubuntu20.*)
+case "$ID-$VERSION_ID" in
+ubuntu-20.*)
   apt-get install -y git cmake libssl-dev zlib1g-dev gcc g++ g++-10
   apt-get install -y file bsdmainutils
   ;;
-ubuntu22.* | debian11)
+ubuntu-22.* | debian-11)
   apt-get install -y git cmake libssl-dev zlib1g-dev gcc g++
   apt-get install -y file bsdmainutils
   ;;
-fedora*)
+fedora-*)
   dnf install -y git gcc-g++ cmake openssl-devel zlib-devel
   dnf install -y glibc-static file libstdc++-static diffutils
   ;;
-opensuse-leap*)
+opensuse-leap-*)
   zypper install -y git make cmake zlib-devel libopenssl-devel gcc-c++ gcc11-c++
   zypper install -y glibc-devel-static tar diffutils util-linux
   ;;
-opensuse-tumbleweed*)
+opensuse-tumbleweed-*)
   zypper install -y git make cmake zlib-devel libopenssl-devel gcc-c++
   zypper install -y glibc-devel-static tar diffutils util-linux
   ;;
-gentoo*)
+gentoo-*)
   emerge dev-vcs/git dev-util/cmake sys-libs/zlib
   ;;
-arch*)
+arch-*)
   pacman -S --needed --noconfirm base-devel zlib openssl cmake util-linux git
   ;;
 *)
