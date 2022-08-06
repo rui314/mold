e30e8e363a1b9535a446798214651090b4e260c8
Rui Ueyama <ruiu@bluewhale.systems>
2022-08-06 08:57:24 +0800

    `git` is no longer a build-time dependency

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -32,7 +32,7 @@ gentoo-*)
   emerge dev-util/cmake sys-libs/zlib
   ;;
 arch-*)
-  pacman -S --needed --noconfirm base-devel zlib openssl cmake util-linux git
+  pacman -S --needed --noconfirm base-devel zlib openssl cmake util-linux
   ;;
 *)
   echo "Error: don't know anything about build dependencies on $ID-$VERSION_ID"
