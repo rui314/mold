ee68d1f2ff2ea5f2bb563fc9f1c00b3efaf691e1
Rui Ueyama <ruiu@bluewhale.systems>
2022-08-11 21:24:14 +0800

    Add a SHA256 fallback implementation
    We are going to use this implementation on Windows.

--- sha.h
+++ sha.h
@@ -1,4 +1,6 @@
-#ifdef __APPLE__
+#if defined(USE_VENDORERD_SHA256)
+#  include "third-party/sha256/sha256.h"
+#elif defined(__APPLE__)
 #  define COMMON_DIGEST_FOR_OPENSSL
 #  include <CommonCrypto/CommonDigest.h>
 #  define SHA256(data, len, md) CC_SHA256(data, len, md)
