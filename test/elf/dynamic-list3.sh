f77220c9fa309cc7affe181109a9b77b9dfd7fc2
Andrew Kaster <akaster@serenityos.org>
2022-08-13 11:22:29 -0600

    test: Add missing tests to CMakeLists
    These tests were added to the repo, but not the list of tests in the
    corresponding CMakeLists. As a result, they are not run in CI when using
    CTest.
    
    Signed-off-by: Andrew Kaster <akaster@serenityos.org>

--- test/elf/dynamic-list3.sh
+++ test/elf/dynamic-list3.sh
@@ -9,7 +9,7 @@ OBJDUMP="${OBJDUMP:-objdump}"
 MACHINE="${MACHINE:-$(uname -m)}"
 testname=$(basename "$0" .sh)
 echo -n "Testing $testname ... "
-t=out/test/elf/$testname
+t=out/test/elf/$MACHINE/$testname
 mkdir -p $t
 
 cat <<'EOF' > $t/dyn
