rui314/mold

commit 0a8431b930c762eab20496b3f60149e969af4b41

Fix an assertion failure for -flto and -dead_strip_dylibs
Fixes https://github.com/rui314/mold/issues/620

macho/input-files.cc
@@ -657,17 +657,17 @@ ObjectFile<E>::mark_live_objects(Context<E> &ctx,
       continue;
 
     if (msym.is_undef() || (msym.is_common() && !sym.is_common))
-      if (InputFile<E> *file = sym.file;
-          !file->is_alive.exchange(true) && !file->is_dylib)
-        feeder((ObjectFile<E> *)file);
+      if (InputFile<E> *file = sym.file)
+        if (file->is_alive.exchange(true) && !file->is_dylib)
+          feeder((ObjectFile<E> *)file);
   }
 
   for (Subsection<E> *subsec : subsections)
     for (UnwindRecord<E> &rec : subsec->get_unwind_records())
       if (Symbol<E> *sym = rec.personality)
-        if (InputFile<E> *file = sym->file;
-            !file->is_alive.exchange(true) && !file->is_dylib)
-          feeder((ObjectFile<E> *)file);
+        if (InputFile<E> *file = sym->file)
+          if (!file->is_alive.exchange(true) && !file->is_dylib)
+            feeder((ObjectFile<E> *)file);
 }
 
 template <typename E>

macho/lto-unix.cc
@@ -140,8 +140,6 @@ void do_lto(Context<E> &ctx) {
     }
   }
 
-  std::erase_if(ctx.objs, [](InputFile<E> *file) { return !file->is_alive; });
-
   // Add a result of LTO as a new object file.
   MappedFile<Context<E>> *mf = new MappedFile<Context<E>>;
   mf->name = "<LTO>";

macho/main.cc
@@ -41,12 +41,16 @@ template <typename E>
 static void resolve_symbols(Context<E> &ctx) {
   Timer t(ctx, "resolve_symbols");
 
-  auto for_each_file = [&](std::function<void(InputFile<E> *)> fn) {
-    tbb::parallel_for_each(ctx.objs, fn);
-    tbb::parallel_for_each(ctx.dylibs, fn);
-  };
+  std::vector<InputFile<E> *> files;
+  append(files, ctx.objs);
+  append(files, ctx.dylibs);
 
-  for_each_file([&](InputFile<E> *file) { file->resolve_symbols(ctx); });
+  tbb::parallel_for_each(files, [&](InputFile<E> *file) {
+    file->resolve_symbols(ctx);
+  });
+
+  if (has_lto_obj(ctx))
+    do_lto(ctx);
 
   for (std::string_view name : ctx.arg.u)
     if (InputFile<E> *file = get_symbol(ctx, name)->file)
@@ -67,18 +71,13 @@ static void resolve_symbols(Context<E> &ctx) {
   }
 
   // Remove symbols of eliminated files.
-  for_each_file([&](InputFile<E> *file) {
+  tbb::parallel_for_each(files, [&](InputFile<E> *file) {
     if (!file->is_alive)
       file->clear_symbols();
   });
 
   std::erase_if(ctx.objs, [](InputFile<E> *file) { return !file->is_alive; });
   std::erase_if(ctx.dylibs, [](InputFile<E> *file) { return !file->is_alive; });
-
-  for_each_file([&](InputFile<E> *file) { file->resolve_symbols(ctx); });
-
-  if (has_lto_obj(ctx))
-    do_lto(ctx);
 }
 
 template <typename E>

test/macho/CMakeLists.txt
@@ -46,6 +46,7 @@ set(MOLD_MACHO_TESTS
   libunwind
   linker-optimization-hints
   lto
+  lto-dead-strip-dylibs
   macos-version-min
   map
   merge-scope

test/macho/lto-dead-strip-dylibs.sh
@@ -1,0 +1,25 @@
+#!/bin/bash
+export LC_ALL=C
+set -e
+CC="${TEST_CC:-cc}"
+CXX="${TEST_CXX:-c++}"
+GCC="${TEST_GCC:-gcc}"
+GXX="${TEST_GXX:-g++}"
+OBJDUMP="${OBJDUMP:-objdump}"
+MACHINE="${MACHINE:-$(uname -m)}"
+testname=$(basename "$0" .sh)
+echo -n "Testing $testname ... "
+t=out/test/macho/$MACHINE/$testname
+mkdir -p $t
+
+cat <<EOF | clang++ -o $t/a.o -c -xc++ - -flto
+#include <iostream>
+int main() {
+  std::cout << "Hello world\n";
+}
+EOF
+
+clang++ --ld-path=./ld64 -o $t/exe $t/a.o -flto -dead_strip_dylibs
+$t/exe | grep -q 'Hello world'
+
+echo OK

commit 6b27a7e9b3f213b98ddf408f276091cf5a2c800a

Fix a regression introduced in 0a8431b930c762eab20496b3f60149e969af4b41
That change made test/macho/all-load.sh to hang up.

macho/input-files.cc
@@ -658,7 +658,7 @@ ObjectFile<E>::mark_live_objects(Context<E> &ctx,
 
     if (msym.is_undef() || (msym.is_common() && !sym.is_common))
       if (InputFile<E> *file = sym.file)
-        if (file->is_alive.exchange(true) && !file->is_dylib)
+        if (!file->is_alive.exchange(true) && !file->is_dylib)
           feeder((ObjectFile<E> *)file);
   }
 