4636580508d2b183fe7486b6c22c036857851f78
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-08-19 14:22:51 +0800

    [Mach-O] Add a test to cover version flags
    Signed-off-by: Zhong Ruoyu <zhongruoyu@outlook.com>

---
+++ test/macho/version.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+export LC_ALL=C
+set -e
+CC="${TEST_CC:-cc}"
+CXX="${TEST_CXX:-c++}"
+GCC="${TEST_GCC:-gcc}"
+GXX="${TEST_GXX:-g++}"
+OBJDUMP="${OBJDUMP:-objdump}"
+MACHINE="${MACHINE:-$(uname -m)}"
+testname=$(basename "$0" .sh)
+echo -n "Testing $testname ... "
+t=out/test/macho/$MACHINE/$testname
+mkdir -p $t
+
+./ld64 -v | grep -q 'mold .*compatible with GNU ld'
+./ld64 --version | grep -q 'mold .*compatible with GNU ld'
+
+cat <<EOF | $CC -o $t/a.o -c -xc -
+#include <stdio.h>
+
+int main() {
+  printf("Hello world\n");
+}
+EOF
+
+rm -f $t/exe
+clang --ld-path=./ld64 -Wl,--version -o $t/exe $t/a.o 2>&1 | grep -q mold
+! [ -f $t/exe ] || false
+
+clang --ld-path=./ld64 -Wl,-v -o $t/exe $t/a.o 2>&1 | grep -q mold
+$QEMU $t/exe | grep -q 'Hello world'
+
+! ./ld64 --v >& $t/log
+grep -q 'unknown command line option:' $t/log
+
+echo OK
