a817f8d5761ec1a6f0d423bc928a7e45334a112b
Rui Ueyama <ruiu@bluewhale.systems>
2022-08-20 08:54:16 +0800

    Enable mold to be built under BSDs
    This commit is imported from
    https://github.com/oneapi-src/oneTBB/commit/c4a42feb5333488360617a9b58f90e479659b744
    
    Fixes https://github.com/rui314/mold/issues/549

--- third-party/tbb/cmake/compilers/Clang.cmake
+++ third-party/tbb/cmake/compilers/Clang.cmake
@@ -44,7 +44,7 @@ if (NOT TBB_STRICT AND COMMAND tbb_remove_compile_flag)
 endif()
 
 # Enable Intel(R) Transactional Synchronization Extensions (-mrtm) and WAITPKG instructions support (-mwaitpkg) on relevant processors
-if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86|AMD64|i.86)")
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86|AMD64|amd64|i.86)")
     set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -mrtm $<$<NOT:$<VERSION_LESS:${CMAKE_CXX_COMPILER_VERSION},12.0>>:-mwaitpkg>)
 endif()
 
