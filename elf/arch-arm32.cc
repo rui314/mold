05f8bcff314fb0e9845746efe0dab827c618599e
Rui Ueyama <ruiu@bluewhale.systems>
2022-09-04 09:56:38 +0800

    Improve comments

--- elf/arch-arm32.cc
+++ elf/arch-arm32.cc
@@ -20,10 +20,13 @@
 //
 // You can distinguish Thumb functions from ARM functions by looking
 // at the least significant bit (LSB) of its "address". If LSB is 0,
-// it's ARM; otherwise, Thumb. LSB is not a part of its real address.
+// it's ARM; otherwise, Thumb.
+//
 // For example, if a symbol `foo` is of type STT_FUNC and has value
-// 0x2001, then `foo` is a function using Thumb instructions whose
-// address is 0x2000 (not 0x2001).
+// 0x2001, `foo` is a function using Thumb instructions whose address
+// is 0x2000 (not 0x2001, as Thumb instructions are always 2-byte
+// aligned). Likewise, if a function pointer has value 0x2001, it
+// refers a Thumb function at 0x2000.
 
 #include "mold.h"
 
