410e1633e2c17a70e83aa94ca93a9f884f3d1b4b
Yang Liu <liuyang22@iscas.ac.cn>
2022-09-08 16:58:59 +0800

    Add a no-eh-frame-header test
    Signed-off-by: Yang Liu <liuyang22@iscas.ac.cn>

---
+++ test/elf/no-eh-frame-header.sh
@@ -0,0 +1,28 @@
++#!/bin/bash
+export LC_ALL=C
+set -e
+CC="${TEST_CC:-cc}"
+CXX="${TEST_CXX:-c++}"
+GCC="${TEST_GCC:-gcc}"
+GXX="${TEST_GXX:-g++}"
+OBJDUMP="${OBJDUMP:-objdump}"
+MACHINE="${MACHINE:-$(uname -m)}"
+testname=$(basename "$0" .sh)
+echo -n "Testing $testname ... "
+t=out/test/elf/$MACHINE/$testname
+mkdir -p $t
+
+cat <<EOF | $CC -o $t/a.o -c -xc -
+int main() {
+  return 0;
+}
+EOF
+
+$CC -B. -Wl,--no-eh-frame-hdr -Wl,--thread-count=1 -O0 -o $t/exe $t/a.o
+
+$OBJDUMP -D $t/exe > $t/log
+! grep -q ".eh_frame_hdr" $t/log || false
+
+$QEMU $t/exe
+
+echo OK
