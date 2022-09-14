05c2eb3790c55dc8128af031c9fc80e808f1df0f
Martin Liska <mliska@suse.cz>
2022-09-14 21:45:43 +0200

    tests: come up with common base.sh
    All tests have a common start, factor it out.
    
    Signed-off-by: Martin Liska <mliska@suse.cz>

--- test/elf/linker-script-defsym.sh
+++ test/elf/linker-script-defsym.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-export LC_ALL=C
-set -e
-CC="${TEST_CC:-cc}"
-CXX="${TEST_CXX:-c++}"
-GCC="${TEST_GCC:-gcc}"
-GXX="${TEST_GXX:-g++}"
-OBJDUMP="${OBJDUMP:-objdump}"
-MACHINE="${MACHINE:-$(uname -m)}"
-testname=$(basename "$0" .sh)
-echo -n "Testing $testname ... "
-t=out/test/elf/$MACHINE/$testname
-mkdir -p $t
+dirname=$(dirname "$0")
+source $dirname/base.sh
 
 cat <<EOF | $CC -o $t/a.o -c -xc -
 int foo() { return 42; }
