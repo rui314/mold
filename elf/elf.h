ce78dfd4b2afab1b501f7fc4b92b53be27e61e96
Rui Ueyama <ruiu@bluewhale.systems>
2022-09-16 21:38:48 +0800

    Simplify

--- elf/elf.h
+++ elf/elf.h
@@ -1562,7 +1562,6 @@ struct EL64Sym {
   bool is_undef_weak() const { return is_undef() && is_weak(); }
 
   ul32 st_name;
-
   u8 st_type : 4;
   u8 st_bind : 4;
   u8 st_visibility : 2;
@@ -2025,25 +2024,25 @@ struct EBNhdr {
 //
 
 template <typename E>
-using U64 = std::conditional_t<E::is_le, ul64, ub64>;
-
-template <typename E>
-using U32 = std::conditional_t<E::is_le, ul32, ub32>;
-
-template <typename E>
-using U24 = std::conditional_t<E::is_le, ul24, ub24>;
-
-template <typename E>
-using U16 = std::conditional_t<E::is_le, ul16, ub16>;
-
-template <typename E>
-using I64 = std::conditional_t<E::is_le, il64, ib64>;
+using I16 = std::conditional_t<E::is_le, il16, ib16>;
 
 template <typename E>
 using I32 = std::conditional_t<E::is_le, il32, ib32>;
 
 template <typename E>
-using I16 = std::conditional_t<E::is_le, il16, ib16>;
+using I64 = std::conditional_t<E::is_le, il64, ib64>;
+
+template <typename E>
+using U16 = std::conditional_t<E::is_le, ul16, ub16>;
+
+template <typename E>
+using U24 = std::conditional_t<E::is_le, ul24, ub24>;
+
+template <typename E>
+using U32 = std::conditional_t<E::is_le, ul32, ub32>;
+
+template <typename E>
+using U64 = std::conditional_t<E::is_le, ul64, ub64>;
 
 template <typename E>
 using Word = std::conditional_t<E::is_64, U64<E>, U32<E>>;
