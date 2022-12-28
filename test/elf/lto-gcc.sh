rui314/mold

commit 804b8439833604981a55d56346e5d1a86e43823c

support -ffat-lto-objects for GCC compiler
FAT LTO objects are objects that contain both native assembly
and LTO bytecode. Identify such files as GCC_LTO_OBJ.

Signed-off-by: Martin Liska <mliska@suse.cz>

filetype.h
@@ -36,9 +36,26 @@ inline bool is_gcc_lto_obj(MappedFile<C> *mf) {
 
   const char *data = mf->get_contents().data();
   ElfEhdr<E> &ehdr = *(ElfEhdr<E> *)data;
+  ElfShdr<E> *sh_begin = (ElfShdr<E> *)(data + ehdr.e_shoff);
   std::span<ElfShdr<E>> shdrs{(ElfShdr<E> *)(data + ehdr.e_shoff), ehdr.e_shnum};
 
+  // e_shstrndx is a 16-bit field. If .shstrtab's section index is
+  // too large, the actual number is stored to sh_link field.
+  i64 shstrtab_idx = (ehdr.e_shstrndx == SHN_XINDEX)
+    ? sh_begin->sh_link : ehdr.e_shstrndx;
+
   for (ElfShdr<E> &sec : shdrs) {
+    // GCC LTO object contains only sections symbols followed by a common
+    // symbol whose name is `__gnu_lto_slim` (or `__gnu_lto_v1` for older
+    // GCC releases).
+    //
+    // However, FAT LTO objects don't have any of the above mentioned symbols
+    // and can identify LTO by `.gnu.lto_.symtab.` section, similarly
+    // to what lto-plugin does.
+    std::string_view name = data + shdrs[shstrtab_idx].sh_offset + sec.sh_name;
+    if (name.starts_with (".gnu.lto_.symtab."))
+      return true;
+
     if (sec.sh_type != SHT_SYMTAB)
       continue;
 
@@ -49,8 +66,6 @@ inline bool is_gcc_lto_obj(MappedFile<C> *mf) {
       return type == STT_NOTYPE || type == STT_FILE || type == STT_SECTION;
     };
 
-    // GCC LTO object contains only sections symbols followed by a common
-    // symbol whose name is `__gnu_lto_v1` or `__gnu_lto_slim`.
     i64 i = 1;
     while (i < elf_syms.size() && skip(elf_syms[i].st_type))
       i++;

test/elf/lto-gcc.sh
@@ -13,3 +13,12 @@ EOF
 
 $GCC -B. -o $t/exe -flto $t/a.o
 $QEMU $t/exe | grep -q 'Hello world'
+
+# Test that LTO is used for FAT LTO objects
+cat <<EOF | $GCC -flto -ffat-lto-objects -c -o $t/a.o -xc -
+#include <stdio.h>
+int main() {
+  printf("Hello world\n");
+}
+EOF
+$GCC -B. -o $t/exe $t/a.o --verbose 2>&1 | grep -- -fwpa

commit 5f99006de3074e878eaa569a20a8efacff910311

support correctly -fno-use-linker-plugin
Right now, mold linker does not work with -fno-use-linker-plugin
and thus use assembly for GCC FAT LTO objects. I noticed that
when running GCC test-suite with mold.

Fixes: 804b8439833604981a55d56346e5d1a86e43823c

Signed-off-by: Martin Liska <mliska@suse.cz>

archive-file.h
@@ -166,7 +166,7 @@ read_fat_archive_members(C &ctx, MappedFile<C> *mf) {
 template <typename C>
 std::vector<MappedFile<C> *>
 read_archive_members(C &ctx, MappedFile<C> *mf) {
-  switch (get_file_type(mf)) {
+  switch (get_file_type(mf, !ctx.arg.plugin.empty())) {
   case FileType::AR:
     return read_fat_archive_members(ctx, mf);
   case FileType::THIN_AR:

elf/main.cc
@@ -73,19 +73,21 @@ MachineType get_machine_type(Context<E> &ctx, MappedFile<Context<E>> *mf) {
     }
   };
 
-  switch (get_file_type(mf)) {
+  bool opt_plugin = !ctx.arg.plugin.empty();
+
+  switch (get_file_type(mf, opt_plugin)) {
   case FileType::ELF_OBJ:
   case FileType::ELF_DSO:
   case FileType::GCC_LTO_OBJ:
     return get_elf_type(mf->data);
   case FileType::AR:
     for (MappedFile<Context<E>> *child : read_fat_archive_members(ctx, mf))
-      if (get_file_type(child) == FileType::ELF_OBJ)
+      if (get_file_type(child, opt_plugin) == FileType::ELF_OBJ)
         return get_elf_type(child->data);
     return MachineType::NONE;
   case FileType::THIN_AR:
     for (MappedFile<Context<E>> *child : read_thin_archive_members(ctx, mf))
-      if (get_file_type(child) == FileType::ELF_OBJ)
+      if (get_file_type(child, opt_plugin) == FileType::ELF_OBJ)
         return get_elf_type(child->data);
     return MachineType::NONE;
   case FileType::TEXT:
@@ -159,7 +161,8 @@ void read_file(Context<E> &ctx, MappedFile<Context<E>> *mf) {
   if (ctx.visited.contains(mf->name))
     return;
 
-  FileType type = get_file_type(mf);
+  bool opt_plugin = !ctx.arg.plugin.empty();
+  FileType type = get_file_type(mf, opt_plugin);
   switch (type) {
   case FileType::ELF_OBJ:
     ctx.objs.push_back(new_object_file(ctx, mf, ""));
@@ -171,7 +174,7 @@ void read_file(Context<E> &ctx, MappedFile<Context<E>> *mf) {
   case FileType::AR:
   case FileType::THIN_AR:
     for (MappedFile<Context<E>> *child : read_archive_members(ctx, mf)) {
-      switch (get_file_type(child)) {
+      switch (get_file_type(child, opt_plugin)) {
       case FileType::ELF_OBJ:
         ctx.objs.push_back(new_object_file(ctx, child, mf->name));
         break;

filetype.h
@@ -31,7 +31,7 @@ bool is_text_file(MappedFile<C> *mf) {
 }
 
 template <typename E, typename C>
-inline bool is_gcc_lto_obj(MappedFile<C> *mf) {
+inline bool is_gcc_lto_obj(MappedFile<C> *mf, bool opt_plugin) {
   using namespace mold::elf;
 
   const char *data = mf->get_contents().data();
@@ -51,9 +51,10 @@ inline bool is_gcc_lto_obj(MappedFile<C> *mf) {
     //
     // However, FAT LTO objects don't have any of the above mentioned symbols
     // and can identify LTO by `.gnu.lto_.symtab.` section, similarly
-    // to what lto-plugin does.
+    // to what lto-plugin does. However, if LTO linker plug-in is not available,
+    // use the emitted assembly instead.
     std::string_view name = data + shdrs[shstrtab_idx].sh_offset + sec.sh_name;
-    if (name.starts_with (".gnu.lto_.symtab."))
+    if (opt_plugin && name.starts_with (".gnu.lto_.symtab."))
       return true;
 
     if (sec.sh_type != SHT_SYMTAB)
@@ -83,7 +84,7 @@ inline bool is_gcc_lto_obj(MappedFile<C> *mf) {
 }
 
 template <typename C>
-FileType get_file_type(MappedFile<C> *mf) {
+FileType get_file_type(MappedFile<C> *mf, bool opt_plugin) {
   std::string_view data = mf->get_contents();
 
   if (data.empty())
@@ -97,10 +98,10 @@ FileType get_file_type(MappedFile<C> *mf) {
 
       if (ehdr.e_type == elf::ET_REL) {
         if (ehdr.e_ident[elf::EI_CLASS] == elf::ELFCLASS32) {
-          if (is_gcc_lto_obj<elf::I386>(mf))
+          if (is_gcc_lto_obj<elf::I386>(mf, opt_plugin))
             return FileType::GCC_LTO_OBJ;
         } else {
-          if (is_gcc_lto_obj<elf::X86_64>(mf))
+          if (is_gcc_lto_obj<elf::X86_64>(mf, opt_plugin))
             return FileType::GCC_LTO_OBJ;
         }
         return FileType::ELF_OBJ;
@@ -113,10 +114,10 @@ FileType get_file_type(MappedFile<C> *mf) {
 
       if (ehdr.e_type == elf::ET_REL) {
         if (ehdr.e_ident[elf::EI_CLASS] == elf::ELFCLASS32) {
-          if (is_gcc_lto_obj<elf::M68K>(mf))
+          if (is_gcc_lto_obj<elf::M68K>(mf, opt_plugin))
             return FileType::GCC_LTO_OBJ;
         } else {
-          if (is_gcc_lto_obj<elf::SPARC64>(mf))
+          if (is_gcc_lto_obj<elf::SPARC64>(mf, opt_plugin))
             return FileType::GCC_LTO_OBJ;
         }
         return FileType::ELF_OBJ;

test/elf/lto-gcc.sh
@@ -23,3 +23,15 @@ int main() {
 EOF
 
 $GCC -B. -o $t/exe2 $t/b.o --verbose 2>&1 | grep -q -- -fwpa
+
+# Test FAT objects if -fno-use-linker-plugin is used
+
+cat <<EOF | $GCC -flto -fno-use-linker-plugin -c -o $t/c.o -xc -
+#include <stdio.h>
+int main() {
+  printf("Hello world\n");
+}
+EOF
+
+$GCC -B. -o $t/exe3 -flto -fno-use-linker-plugin $t/c.o
+$QEMU $t/exe3 | grep -q 'Hello world'