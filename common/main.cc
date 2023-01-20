dbeb10c0d2e1762fdc0e2c48720d76b74f5474bc
Rui Ueyama <ruiu@bluewhale.systems>
2023-01-20 16:21:18 +0800

    Recognize ld64.sold as a command name

--- common/main.cc
+++ common/main.cc
@@ -148,7 +148,7 @@ int main(int argc, char **argv) {
 
 #if MOLD_IS_SOLD
   std::string cmd = mold::filepath(argv[0]).filename().string();
-  if (cmd == "ld64" || cmd == "ld64.mold")
+  if (cmd == "ld64" || cmd == "ld64.mold" || cmd == "ld64.sold")
     return mold::macho::main(argc, argv);
 #endif
 
