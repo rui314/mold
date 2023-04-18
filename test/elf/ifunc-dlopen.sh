ceffcfcadd9958343563531951eaf8324bc4c878
Rui Ueyama <ruiu@bluewhale.systems>
2023-04-18 16:43:41 +0900

    Fix test
    Fixes https://github.com/rui314/mold/issues/1030

--- test/elf/ifunc-dlopen.sh
+++ test/elf/ifunc-dlopen.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 . $(dirname $0)/common.inc
 
+supports_ifunc || skip
+
 cat <<EOF | $CC -fno-PIE -o $t/a.o -c -xc -
 #include <dlfcn.h>
 #include <stdio.h>
-#include <string.h>
+
+typedef void Func();
+void foo(void);
 
 int main() {
   void *handle = dlopen(NULL, RTLD_NOW);
-  void *p = dlsym(handle, "memcpy");
-  printf("%p %p\n", memcpy, p);
+  Func *p = dlsym(handle, "foo");
+
+  foo();
+  p();
+  printf("%p %p\n", foo, p);
 }
 EOF
 
-$CC -B. -o $t/exe $t/a.o -no-pie -ldl
-$QEMU $t/exe | grep -Eq '^(.+) \1$'
+cat <<EOF | $CC -fPIC -o $t/b.o -c -xc -
+#include <stdio.h>
+
+__attribute__((ifunc("resolve_foo")))
+void foo(void);
+
+static void real_foo(void) {
+  printf("foo ");
+}
+
+typedef void Func();
+
+static Func *resolve_foo(void) {
+  return real_foo;
+}
+EOF
+
+$CC -B. -o $t/c.so $t/b.o -shared
+$CC -B. -o $t/exe $t/a.o $t/c.so -no-pie -ldl
+$QEMU $t/exe | grep -q 'foo foo'
