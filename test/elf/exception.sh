f2b278ce10071667128fd0e8feed8b1bef86ee9f
Rui Ueyama <ruiu@bluewhale.systems>
2023-07-30 21:59:58 +0900

    Attempt to fix a test for Alt Linux
    Fixes https://github.com/rui314/mold/issues/1073

--- test/elf/exception.sh
+++ test/elf/exception.sh
@@ -26,7 +26,7 @@ $CXX -c -o $t/c.o $t/a.cc -fno-PIC
 $CXX -B. -o $t/exe1 $t/b.o $static
 $QEMU $t/exe1
 
-$CXX -B. -o $t/exe2 $t/c.o $static
+$CXX -B. -o $t/exe2 $t/c.o -no-pie $static
 $QEMU $t/exe2
 
 $CXX -B. -o $t/exe3 $t/b.o -pie
@@ -38,7 +38,7 @@ $QEMU $t/exe4
 $CXX -B. -o $t/exe5 $t/b.o -pie -Wl,--gc-sections
 $QEMU $t/exe5
 
-$CXX -B. -o $t/exe6 $t/c.o $static -Wl,--gc-sections
+$CXX -B. -o $t/exe6 $t/c.o -no-pie $static -Wl,--gc-sections
 $QEMU $t/exe6
 
 if [ $MACHINE = x86_64 ]; then
