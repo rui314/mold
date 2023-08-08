d752bac24a8d78d87c54fcc2fa17182fa4958102
Rui Ueyama <ruiu@bluewhale.systems>
2023-08-08 17:46:45 +0900

    Do not handle --strip-debug as a synonym for --strip-all
    Fixes https://github.com/rui314/mold/issues/1080

---
+++ test/elf/strip-debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+. $(dirname $0)/common.inc
+
+cat <<EOF | $CC -o $t/a.o -c -xc - -g
+static void foo() {}
+void bar() {}
+int main() {}
+EOF
+
+$CC -B. -o $t/exe $t/a.o -Wl,--strip-debug
+
+readelf -W --sections $t/exe > $t/log
+! grep -Fq .debug_info $t/log || false
+
+readelf -W --symbols $t/exe | grep -q ' bar$'
