fad4c1b56cad1bb961bf094b85c5aa007abff4f0
Rui Ueyama <ruiu@bluewhale.systems>
2023-08-19 13:45:33 +0900

    Disable tests in the bundled zlib

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -12,42 +12,42 @@ set -x
 case "$ID-$VERSION_ID" in
 ubuntu-20.* | pop-20.*)
   apt-get update
-  apt-get install -y cmake zlib1g-dev gcc g++ g++-10
+  apt-get install -y cmake gcc g++ g++-10
   apt-get install -y file
   ;;
 ubuntu-* | pop-* | linuxmint-* | debian-* | raspbian-*)
   apt-get update
-  apt-get install -y cmake zlib1g-dev gcc g++
+  apt-get install -y cmake gcc g++
   apt-get install -y file
   ;;
 fedora-*)
-  dnf install -y gcc-g++ cmake zlib-devel
+  dnf install -y gcc-g++ cmake
   dnf install -y glibc-static file libstdc++-static diffutils util-linux
   ;;
 opensuse-leap-*)
-  zypper install -y make cmake zlib-devel gcc-c++ gcc11-c++
+  zypper install -y make cmake gcc-c++ gcc11-c++
   zypper install -y glibc-devel-static tar diffutils util-linux
   ;;
 opensuse-tumbleweed-*)
-  zypper install -y make cmake zlib-devel gcc-c++
+  zypper install -y make cmake gcc-c++
   zypper install -y glibc-devel-static tar diffutils util-linux
   ;;
 gentoo-*)
   emerge-webrsync
-  emerge dev-util/cmake sys-libs/zlib
+  emerge dev-util/cmake
   ;;
 arch-*)
   pacman -Sy
-  pacman -S --needed --noconfirm base-devel zlib cmake util-linux
+  pacman -S --needed --noconfirm base-devel cmake util-linux
   ;;
 void-*)
   xbps-install -Sy xbps
-  xbps-install -Sy bash make cmake zlib-devel gcc
+  xbps-install -Sy bash make cmake gcc
   xbps-install -Sy tar diffutils util-linux
   ;;
 alpine-*)
   apk update
-  apk add bash make linux-headers cmake zlib-dev gcc g++
+  apk add bash make linux-headers cmake gcc g++
   ;;
 *)
   echo "Error: don't know anything about build dependencies on $ID-$VERSION_ID"
