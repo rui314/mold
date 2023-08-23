63df2a3c8b18afb188de463d86c4e9df66e3f670
Rui Ueyama <ruiu@bluewhale.systems>
2023-08-23 19:45:28 +0900

    Add a script to install cross toolchains

---
+++ install-cross-tools.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+. /etc/os-release
+
+set -x
+
+# This script install packages for -DMOLD_ENABLE_QEMU_TESTS=1
+# to enable cross-target tests.
+#
+# Feel free to send me a PR if your OS is not on this list.
+
+case "$ID-$VERSION_ID" in
+ubuntu-* | pop-* | linuxmint-* | debian-* | raspbian-*)
+  apt-get install -y qemu-user {gcc,g++}-{i686,aarch64,riscv64,powerpc,powerpc64,powerpc64le,s390x,sparc64,m68k,sh4,alpha,mips,mipsel}-linux-gnu {gcc,g++}-arm-linux-gnueabihf {gcc,g++}-{mips64,mips64el}-linux-gnuabi64
+  ;;
+*)
+  echo "Error: don't know anything about build dependencies on $ID-$VERSION_ID"
+  exit 1
+esac
