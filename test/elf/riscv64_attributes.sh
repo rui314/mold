f76d3e0d0c285fab4d770d4b57035d57690fade7
Rui Ueyama <ruiu@bluewhale.systems>
2023-08-31 18:16:26 +0900

    Fix a unit test
    Recent versions of GCC report a compile error if an extension
    name is not recognizable.
    
    Fixes https://github.com/rui314/mold/issues/1097

--- test/elf/riscv64_attributes.sh
+++ test/elf/riscv64_attributes.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 . $(dirname $0)/common.inc
 
-cat <<EOF | $CC -march=rv64imafd_xfoo1p5 -o $t/a.o -c -xc -
+cat <<EOF | $CC -march=rv64imafd_xtheadba1p5 -o $t/a.o -c -xc -
 void foo() {}
 EOF
 
 # The compiler might not create .riscv.attributes
 readelf --sections $t/a.o | grep -Fq .riscv.attributes || skip
 
-cat <<EOF | $CC -march=rv64imafd_xfoo2p0 -o $t/b.o -c -xc -
+cat <<EOF | $CC -march=rv64imafd_xtheadba2p0 -o $t/b.o -c -xc -
 void bar() {}
 EOF
 
@@ -17,7 +17,7 @@ void _start() {}
 EOF
 
 $CC -B. -nostdlib -o $t/exe $t/a.o $t/c.o
-readelf -A $t/exe | grep -q '_xfoo1p5"'
+readelf -A $t/exe | grep -q '_xtheadba1p5"'
 
 $CC -B. -nostdlib -o $t/exe $t/a.o $t/b.o $t/c.o
-readelf -A $t/exe | grep -q '_xfoo2p0"'
+readelf -A $t/exe | grep -q '_xtheadba2p0"'
