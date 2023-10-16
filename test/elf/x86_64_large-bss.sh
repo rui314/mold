d8a4f14fd0b2bed23c0d9649c97ce6b0c2502b00
Rui Ueyama <ruiu@cs.stanford.edu>
2023-10-16 13:55:54 +0900

    Fix CI

--- test/elf/x86_64_large-bss.sh
+++ test/elf/x86_64_large-bss.sh
@@ -3,16 +3,16 @@
 
 [ $MACHINE = x86_64 ] || skip
 
-cat <<EOF | $CC -o $t/a.o -c -xc - 2> /dev/null || skip
+cat <<EOF | $CC -o $t/a.o -c -xc - -mcmodel=large
 #include <stdio.h>
 char arr1[0xc0000000];
 extern char arr2[0xc0000000];
 int main() {
-  printf("%d %lx\n", arr1 < arr2, arr2 - arr1);
+  printf("%d %lx\n", (void *)arr1 < (void *)arr2, arr2 - arr1);
 }
 EOF
 
-cat <<EOF | $CC -o $t/b.o -c -xc - 2> /dev/null || skip
+cat <<EOF | $CC -o $t/b.o -c -xc - -mcmodel=large
 char arr2[0xc0000000];
 EOF
 
