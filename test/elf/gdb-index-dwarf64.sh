c7c1779ad784f1f892389c9157f0660a3659dd23
Rui Ueyama <ruiu@cs.stanford.edu>
2023-10-17 17:04:53 +0900

    Fix CI

--- test/elf/gdb-index-dwarf64.sh
+++ test/elf/gdb-index-dwarf64.sh
@@ -63,10 +63,12 @@ $CC -c -o $t/b.o $t/b.c -fPIC -g -ggnu-pubnames -gdwarf-4 -gdwarf64 -ffunction-s
 $CC -c -o $t/c.o $t/c.c -fPIC -g -ggnu-pubnames -gdwarf-5 -gdwarf64
 $CC -c -o $t/d.o $t/d.c -fPIC -g -ggnu-pubnames -gdwarf-5 -gdwarf64 -ffunction-sections
 
-$CC -B. -shared -o $t/e.so $t/a.o $t/b.o $t/c.o $t/d.o -Wl,--gdb-index
-readelf -WS $t/e.so 2> /dev/null | grep -Fq .gdb_index
+$CC -B. -shared -o $t/e.so $t/a.o $t/b.o $t/c.o $t/d.o
 
-cat <<EOF | $CC -c -o $t/f.o -fPIC -g -ggnu-pubnames -gdwarf-5 -xc - -gz
+$CC -B. -shared -o $t/f.so $t/a.o $t/b.o $t/c.o $t/d.o -Wl,--gdb-index
+readelf -WS $t/f.so 2> /dev/null | grep -Fq .gdb_index
+
+cat <<EOF | $CC -c -o $t/g.o -fPIC -g -ggnu-pubnames -gdwarf-5 -xc - -gz
 void fn1();
 
 int main() {
@@ -74,19 +76,26 @@ int main() {
 }
 EOF
 
-$CC -B. -o $t/exe $t/e.so $t/f.o -Wl,--gdb-index
-readelf -WS $t/exe 2> /dev/null | grep -Fq .gdb_index
+# Older versions of gdb are buggy that they complain DWARF64 debug sections
+# even without .gdb_index. Skip if such version.
+$CC -B. -o $t/exe1 $t/e.so $t/g.o
+DEBUGINFOD_URLS= gdb $t/exe1 -nx -batch -ex 'b main' -ex r -ex quit >& $t/log1
+grep -q 'DW_FORM_line_strp pointing outside of .debug_line_str' $t/log1 && skip
 
-$QEMU $t/exe | grep -q 'Hello world'
+# We are using a recent version of gdb.
+$CC -B. -o $t/exe2 $t/f.so $t/g.o -Wl,--gdb-index
+readelf -WS $t/exe2 2> /dev/null | grep -Fq .gdb_index
 
-DEBUGINFOD_URLS= gdb $t/exe -nx -batch -ex 'b main' -ex r -ex 'b trap' \
-  -ex c -ex bt -ex quit >& $t/log
+$QEMU $t/exe2 | grep -q 'Hello world'
 
-grep -q 'fn8 () at .*/d.c:6' $t/log
-grep -q 'fn7 () at .*/d.c:10' $t/log
-grep -q 'fn6 () at .*/c.c:4' $t/log
-grep -q 'fn5 () at .*/c.c:8' $t/log
-grep -q 'fn4 () at .*/b.c:4' $t/log
-grep -q 'fn3 () at .*/b.c:8' $t/log
-grep -q 'fn2 () at .*/a.c:4' $t/log
-grep -q 'fn1 () at .*/a.c:8' $t/log
+DEBUGINFOD_URLS= gdb $t/exe2 -nx -batch -ex 'b main' -ex r -ex 'b trap' \
+  -ex c -ex bt -ex quit >& $t/log2
+
+grep -q 'fn8 () at .*/d.c:6' $t/log2
+grep -q 'fn7 () at .*/d.c:10' $t/log2
+grep -q 'fn6 () at .*/c.c:4' $t/log2
+grep -q 'fn5 () at .*/c.c:8' $t/log2
+grep -q 'fn4 () at .*/b.c:4' $t/log2
+grep -q 'fn3 () at .*/b.c:8' $t/log2
+grep -q 'fn2 () at .*/a.c:4' $t/log2
+grep -q 'fn1 () at .*/a.c:8' $t/log2
