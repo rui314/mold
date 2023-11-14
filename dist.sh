25ceab07f70cea73e0a0b5dbc974ce69eeb48471
Rui Ueyama <ruiu@cs.stanford.edu>
2023-11-14 11:13:43 +0900

    Build mold on Debian 8 (Jessie)
    Debian 8 was released in April 2015, which is old enough for
    binary distribution. Previously, we used to use Debian 10 (Bullseye),
    which was released in July 2019.

--- dist.sh
+++ dist.sh
@@ -27,6 +27,10 @@
 #
 # We need GCC 10 or newer to build mold. If GCC 10 is not available on an
 # old Debian version, we'll build it ourselves.
+#
+# You may need to run the following command to use Docker with Qemu:
+#
+#  $ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 set -e -x
 
@@ -56,20 +60,20 @@ dest=mold-$version-$arch-linux
 # Create a Docker image.
 case $arch in
 x86_64)
+  # Debian 8 (Jessie) released in April 2015
   cat <<EOF | docker build --platform $arch -t $image -
-FROM debian:buster-20231030
+FROM debian:jessie-20210326
 ENV DEBIAN_FRONTEND=noninteractive TZ=UTC
-RUN echo 'deb http://snapshot.debian.org/archive/debian/20231030T000000Z buster main' > /etc/apt/sources.list && \
-  echo 'deb http://snapshot.debian.org/archive/debian-security/20231030T000000Z buster/updates main' >> /etc/apt/sources.list && \
-  echo 'deb http://snapshot.debian.org/archive/debian/20231030T000000Z buster-updates main' >> /etc/apt/sources.list && \
-  apt-get update -o Acquire::Check-Valid-Until=false && \
-  apt-get install -y wget bzip2 file make autoconf gcc g++ libssl-dev && \
+RUN sed -i -e '/^deb/d' -e 's/^# //g' /etc/apt/sources.list && \
+  echo 'Acquire { Retries "10"; http::timeout "10"; Check-Valid-Until "false"; };' > /etc/apt/apt.conf.d/80-retries && \
+  apt-get update && \
+  apt-get install -y --force-yes --no-install-recommends wget bzip2 file make autoconf gcc g++ libssl-dev && \
   rm -rf /var/lib/apt/lists
 
 # Build CMake 3.27
 RUN mkdir -p /build/cmake && \
   cd /build/cmake && \
-  wget -O- -q https://github.com/Kitware/CMake/releases/download/v3.27.7/cmake-3.27.7.tar.gz | tar xzf - --strip-components=1 && \
+  wget -O- --no-check-certificate https://cmake.org/files/v3.27/cmake-3.27.7.tar.gz | tar xzf - --strip-components=1 && \
   ./bootstrap --parallel=$(nproc) && \
   make -j$(nproc) && \
   make install && \
@@ -78,43 +82,42 @@ RUN mkdir -p /build/cmake && \
 # Build GCC 10
 RUN mkdir -p /build/gcc && \
   cd /build/gcc && \
-  wget -O- -q https://ftp.gnu.org/gnu/gcc/gcc-10.5.0/gcc-10.5.0.tar.gz | tar xzf - --strip-components=1 && \
+  wget -O- http://ftp.gnu.org/gnu/gcc/gcc-10.5.0/gcc-10.5.0.tar.gz | tar xzf - --strip-components=1 && \
   mkdir isl gmp mpc mpfr && \
-  wget -O- -q https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.24.tar.bz2 | tar xjf - --strip-components=1 -C isl && \
-  wget -O- -q https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.gz | tar xzf - --strip-components=1 -C gmp && \
-  wget -O- -q https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz | tar xzf - --strip-components=1 -C mpc && \
-  wget -O- -q https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.1.tar.gz | tar xzf - --strip-components=1 -C mpfr && \
-  ./configure --enable-languages=c,c++ --disable-bootstrap --disable-multilib && \
+  wget -O- http://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2 | tar xjf - --strip-components=1 -C isl && \
+  wget -O- http://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2 | tar xjf - --strip-components=1 -C gmp && \
+  wget -O- http://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz | tar xzf - --strip-components=1 -C mpc && \
+  wget -O- http://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.gz | tar xzf - --strip-components=1 -C mpfr && \
+  ./configure --prefix=/usr --enable-languages=c,c++ --disable-bootstrap --disable-multilib && \
   make -j$(nproc) && \
   make install && \
-  ln -sf /usr/local/bin/gcc /usr/local/bin/cc && \
-  ln -sf /usr/local/bin/g++ /usr/local/bin/c++ && \
+  ln -sf /usr/lib64/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6 && \
   rm -rf /build
 EOF
   ;;
 aarch64 | arm | ppc64le | s390x)
+  # Debian 10 (Bullseye) released in July 2019
   cat <<EOF | docker build --platform $arch -t $image -
 FROM debian:bullseye-20231030
 ENV DEBIAN_FRONTEND=noninteractive TZ=UTC
-RUN echo 'deb http://snapshot.debian.org/archive/debian/20231030T000000Z bullseye main' > /etc/apt/sources.list && \
-  echo 'deb http://snapshot.debian.org/archive/debian-security/20231030T000000Z bullseye-security main' >> /etc/apt/sources.list && \
-  echo 'deb http://snapshot.debian.org/archive/debian/20231030T000000Z bullseye-updates main' >> /etc/apt/sources.list && \
-  apt-get update -o Acquire::Check-Valid-Until=false && \
+RUN sed -i -e '/^deb/d' -e 's/^# //g' /etc/apt/sources.list && \
+  echo 'Acquire { Retries "10"; http::timeout "10"; Check-Valid-Until "false"; };' > /etc/apt/apt.conf.d/80-retries && \
+  apt-get update && \
   apt-get install -y --no-install-recommends build-essential gcc-10 g++-10 cmake && \
-  ln -s /usr/bin/gcc-10 /usr/local/bin/cc && \
-  ln -s /usr/bin/g++-10 /usr/local/bin/c++ && \
+  ln -sf /usr/bin/gcc-10 /usr/bin/cc && \
+  ln -sf /usr/bin/g++-10 /usr/bin/c++ && \
   rm -rf /var/lib/apt/lists
 EOF
   ;;
 riscv64)
   # snapshot.debian.org is not available for RISC-V binaries
   cat <<EOF | docker build --platform $arch -t $image -
-FROM riscv64/debian:sid-20231030
+FROM riscv64/debian:unstable-20231030
 ENV DEBIAN_FRONTEND=noninteractive TZ=UTC
 RUN apt-get update && \
   apt-get install -y --no-install-recommends build-essential gcc-12 g++-12 cmake && \
-  ln -s /usr/bin/gcc-12 /usr/local/bin/cc && \
-  ln -s /usr/bin/g++-12 /usr/local/bin/c++ && \
+  ln -sf /usr/bin/gcc-12 /usr/bin/cc && \
+  ln -sf /usr/bin/g++-12 /usr/bin/c++ && \
   rm -rf /var/lib/apt/lists
 EOF
   ;;
@@ -130,7 +133,8 @@ docker run --platform linux/$arch -i --rm -v "$(realpath $(dirname $0)):/mold" $
 cd /build/mold &&
 cmake -DCMAKE_BUILD_TYPE=Release -DMOLD_MOSTLY_STATIC=On /mold &&
 cmake --build . -j$(nproc) &&
+ctest -j$(nproc) &&
 cmake --install . --prefix $dest --strip &&
 find $dest -print | xargs touch --no-dereference --date='$timestamp' &&
-tar -cf - --sort=name $dest | gzip -9nc > /mold/$dest.tar.gz &&
+find $dest -print | sort | tar -cf - --no-recursion --files-from=- | gzip -9nc > /mold/$dest.tar.gz &&
 chown $(id -u):$(id -g) /mold/$dest.tar.gz"
