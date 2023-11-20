8bb01e1a0e182f600862bb3725a151ad869791e5
Rui Ueyama <ruiu@cs.stanford.edu>
2023-11-20 14:25:03 +0900

    Fix CI

--- test/elf/nmagic.sh
+++ test/elf/nmagic.sh
@@ -11,4 +11,4 @@ $CC -B. -o $t/exe2 $t/a.o -nostdlib
 end1=$(nm $t/exe1 | grep ' end$' | cut -d' ' -f1)
 end2=$(nm $t/exe2 | grep ' end$' | cut -d' ' -f1)
 
-[ $end1 -lt $end2 ]
+[ $((0x$end1)) -lt $((0x$end2)) ]
