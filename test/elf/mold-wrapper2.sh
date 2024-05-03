ed1e3c06a25184552e2c3d7deeaa81157d9ae0d5
Rui Ueyama <ruiu@cs.stanford.edu>
2024-05-03 10:00:37 +0900

    Do not copy mold executable to a test directory
    mold executable can be quite large (~600 MiB with debug info),
    so we don't want to casually copy that file.

--- test/elf/mold-wrapper2.sh
+++ test/elf/mold-wrapper2.sh
@@ -2,12 +2,6 @@
 . $(dirname $0)/common.inc
 
 ldd mold-wrapper.so | grep -q libasan && skip
-
 nm mold | grep -q '__[at]san_init' && skip
 
-rm -rf $t
-mkdir -p $t/bin $t/lib/mold
-cp mold $t/bin
-cp mold-wrapper.so $t/bin
-
-$t/bin/mold -run bash -c 'echo $LD_PRELOAD' | grep -q '/bin/mold-wrapper.so'
+./mold -run bash -c 'echo $LD_PRELOAD' | grep -Fq mold-wrapper.so
