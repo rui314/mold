2649b69e9581772dc0c6e1052cbce13063db1de8
Rui Ueyama <ruiu@cs.stanford.edu>
2024-05-05 22:43:58 +0900

    Fix CI

--- test/elf/gnu-retain.sh
+++ test/elf/gnu-retain.sh
@@ -3,11 +3,14 @@
 
 [ $MACHINE = ppc64 ] && skip
 
-cat <<EOF | $CC -c -o $t/a.o -xc - -ffunction-sections
+cat <<EOF | $CC -c -o $t/a.o -xc - -ffunction-sections 2> /dev/null
  __attribute__((retain)) int foo() {}
 int bar() {}
 int main() {}
 EOF
+
+# Older versions of GCC does not support __attribute__((retain))
+readelf -WS $t/a.o | grep -q '\.text\.foo.*AXR' || skip
 
 $CC -B. -o $t/exe $t/a.o -Wl,-gc-sections
 nm $t/exe > $t/log
