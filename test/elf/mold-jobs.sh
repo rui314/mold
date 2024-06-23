63f26708fd579d045bb7800a42377e78ee457455
Rui Ueyama <ruiu@cs.stanford.edu>
2024-06-23 12:38:59 +0900

    Unbreak daily buildbot

--- test/elf/mold-jobs.sh
+++ test/elf/mold-jobs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 . $(dirname $0)/common.inc
 
+# Looks like lockf doesn't work correctly on qemu-riscv64
+[ $MACHINE = riscv64 -o $MACHINE = riscv32 ] && skip
+
 cat <<EOF | $CC -o $t/a.o -c -xc - -fno-PIE
 #include <stdio.h>
 int main() {
