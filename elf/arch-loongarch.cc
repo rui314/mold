d8b7c48928bffcb17459c27ae049dcb481662528
Xin Wang <wangxin03@loongson.cn>
2024-08-03 10:34:53 +0800

    LoongArch refactor

--- elf/arch-loongarch.cc
+++ elf/arch-loongarch.cc
@@ -399,36 +399,38 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
       }
       break;
     case R_LARCH_GOT_PC_HI20:
-      if (removed_bytes != 0) {
-        // The first instruction of pcalau12i/ld.d has been removed
-        assert(removed_bytes == 4);
-        break;
-      } else {
-        if (i + 3 < rels.size() &&
-          sym.is_local(ctx) &&
-          !sym.is_absolute() &&
-	  !sym.is_ifunc() &&
-          ctx.arg.relax &&
-          rels[i+1].r_type == R_LARCH_RELAX &&
-          rels[i+3].r_type == R_LARCH_RELAX &&
-          rels[i+2].r_type == R_LARCH_GOT_PC_LO12 &&
-          rels[i+2].r_offset == rel.r_offset + 4) {
-          u32 insn1 = *(ul32 *)(contents.data() + rel.r_offset);
-          u32 insn2 = *(ul32 *)(contents.data() + rels[i+2].r_offset);
-          u32 rd = get_rd(insn1);
+      switch (removed_bytes) {
+        // pcalau12i/ld.d has been relaxed to pcaddi, the first insn has been removed.
+        case 4:
+          break;
+        case 0:
+          if (i + 3 < rels.size() &&
+            sym.is_local(ctx) &&
+            !sym.is_absolute() &&
+            !sym.is_ifunc() &&
+            ctx.arg.relax &&
+            rels[i+1].r_type == R_LARCH_RELAX &&
+            rels[i+3].r_type == R_LARCH_RELAX &&
+            rels[i+2].r_type == R_LARCH_GOT_PC_LO12 &&
+            rels[i+2].r_offset == rel.r_offset + 4) {
+            u32 insn1 = *(ul32 *)(contents.data() + rel.r_offset);
+            u32 insn2 = *(ul32 *)(contents.data() + rels[i+2].r_offset);
+            u32 rd = get_rd(insn1);
 
-          if (rd == get_rd(insn2)) {
-            // pcalau12i/ld.d has been relaxed to pcalau12i/addi.d
-            // reloc the pcalau12i as R_LARCH_PLACA_HI20
-            write_j20(loc, hi20(S + A, P));
-            break;
+            if (rd == get_rd(insn2)) {
+              // pcalau12i/ld.d has been relaxed to pcalau12i/addi.d
+              // reloc the pcalau12i as R_LARCH_PLACA_HI20
+              write_j20(loc, hi20(S + A, P));
+              break;
+            }
           }
-        }
 
-        // relax not applied.
-        write_j20(loc, hi20(GOT + G + A, P));
+          // relax not applied.
+          write_j20(loc, hi20(GOT + G + A, P));
+          break;
+        default:
+          unreachable();
       }
-      break;
     case R_LARCH_GOT64_PC_LO20:
       write_j20(loc, higher20(GOT + G + A, P));
       break;
