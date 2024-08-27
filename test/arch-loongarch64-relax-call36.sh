4da257e6fb438b36a50f63bc4742ab88d5331edd
Xin Wang <wangxin03@loongson.cn>
2024-08-27 15:14:56 +0800

    LoongArch: change test of R_LARCH_CALL36 with bigger offset

--- test/arch-loongarch64-relax-call36.sh
+++ test/arch-loongarch64-relax-call36.sh
@@ -5,14 +5,14 @@ cat <<'EOF' | $CC -o $t/a.o -c -xassembler -
 .globl foo, bar
 foo:
   move      $s0,   $ra
-  .reloc ., R_LARCH_CALL36, foo2
+  .reloc ., R_LARCH_CALL36, print
   .reloc ., R_LARCH_RELAX
   pcaddu18i $t0,   0
   jirl      $ra,   $t0, 0
   move      $ra,   $s0
   ret
 bar:
-  .reloc ., R_LARCH_CALL36, bar2
+  .reloc ., R_LARCH_CALL36, print
   .reloc ., R_LARCH_RELAX
   pcaddu18i $t0,   0
   jirl      $zero, $t0, 0
@@ -22,8 +22,10 @@ cat <<EOF | $CC -o $t/b.o -c -xc -
 #include <stdio.h>
 void foo();
 void bar();
-void foo2() { printf("foo"); }
-void bar2() { printf("bar"); }
+void print() __attribute__ ((section(".print")));
+void print() {
+  printf("beef");
+}
 
 int main() {
   foo();
@@ -33,14 +35,14 @@ int main() {
 EOF
 
 $CC -B. -o $t/exe1 $t/a.o $t/b.o -Wl,--no-relax
-$QEMU $t/exe1 | grep -q foobar
+$QEMU $t/exe1 | grep -q beefbeef
 
 $OBJDUMP -d $t/exe1 > $t/exe1.objdump
 grep -A2 '<foo>:' $t/exe1.objdump | grep -wq pcaddu18i
 grep -A2 '<bar>:' $t/exe1.objdump | grep -wq pcaddu18i
 
-$CC -B. -o $t/exe2 $t/a.o $t/b.o -Wl,--relax
-$QEMU $t/exe2 | grep -q foobar
+$CC -B. -o $t/exe2 $t/a.o $t/b.o -Wl,--relax -Wl,-Ttext=0x10000 -Wl,--section-start=.print=0x100000
+$QEMU $t/exe2 | grep -q beefbeef
 
 $OBJDUMP -d $t/exe2 > $t/exe2.objdump
 grep -A2 '<foo>:' $t/exe2.objdump | grep -wq bl
