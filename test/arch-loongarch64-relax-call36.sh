d4c2073cdaae72337c5b29d051eed09e0da800d9
Rui Ueyama <ruiu@cs.stanford.edu>
2024-08-27 20:09:39 +0900

    Fix test
    Previous test passed even without 1c321022d0fc2c9bf1ad8757be7199167a2fcb15.

--- test/arch-loongarch64-relax-call36.sh
+++ test/arch-loongarch64-relax-call36.sh
@@ -3,6 +3,7 @@
 
 cat <<'EOF' | $CC -o $t/a.o -c -xassembler -
 .globl foo, bar
+.space 0x100000
 foo:
   move      $s0,   $ra
   .reloc ., R_LARCH_CALL36, print
@@ -16,15 +17,17 @@ bar:
   .reloc ., R_LARCH_RELAX
   pcaddu18i $t0,   0
   jirl      $zero, $t0, 0
+.space 0x100000
 EOF
 
 cat <<EOF | $CC -o $t/b.o -c -xc -
 #include <stdio.h>
+
 void foo();
 void bar();
-void print() __attribute__ ((section(".print")));
+
 void print() {
-  printf("beef");
+  printf("foo");
 }
 
 int main() {
@@ -35,14 +38,14 @@ int main() {
 EOF
 
 $CC -B. -o $t/exe1 $t/a.o $t/b.o -Wl,--no-relax
-$QEMU $t/exe1 | grep -q beefbeef
+$QEMU $t/exe1 | grep -q foofoo
 
 $OBJDUMP -d $t/exe1 > $t/exe1.objdump
 grep -A2 '<foo>:' $t/exe1.objdump | grep -wq pcaddu18i
 grep -A2 '<bar>:' $t/exe1.objdump | grep -wq pcaddu18i
 
-$CC -B. -o $t/exe2 $t/a.o $t/b.o -Wl,--relax -Wl,-Ttext=0x10000 -Wl,--section-start=.print=0x100000
-$QEMU $t/exe2 | grep -q beefbeef
+$CC -B. -o $t/exe2 $t/a.o $t/b.o -Wl,--relax
+$QEMU $t/exe2 | grep -q foofoo
 
 $OBJDUMP -d $t/exe2 > $t/exe2.objdump
 grep -A2 '<foo>:' $t/exe2.objdump | grep -wq bl
