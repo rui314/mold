rui314/mold

commit 431fc39db00afb6b5dc9ac7260cd49823fa19160

Change how we handle word-size absolute relocations
Word-size absolute relocations, such as R_X86_64_64, are special from
the linker's point of view because only such symbols can be promoted
to dynamic symbols. This patch handles them separately.

src/output-chunks.cc
@@ -404,11 +404,6 @@ void RelDynSection<E>::update_shdr(Context<E> &ctx) {
     offset += chunk->get_reldyn_size(ctx) * sizeof(ElfRel<E>);
   }
 
-  for (ObjectFile<E> *file : ctx.objs) {
-    file->reldyn_offset = offset;
-    offset += file->num_dynrel * sizeof(ElfRel<E>);
-  }
-
   this->shdr.sh_size = offset;
   this->shdr.sh_link = ctx.dynsym->shndx;
 }
@@ -951,14 +946,20 @@ void OutputSection<E>::compute_section_size(Context<E> &ctx) {
 
 template <typename E>
 void OutputSection<E>::copy_buf(Context<E> &ctx) {
-  if (this->shdr.sh_type != SHT_NOBITS)
-    write_to(ctx, ctx.buf + this->shdr.sh_offset);
+  if (this->shdr.sh_type != SHT_NOBITS) {
+    ElfRel<E> *rel = nullptr;
+    if (ctx.reldyn)
+      rel = (ElfRel<E> *)(ctx.buf + ctx.reldyn->shdr.sh_offset +
+                          this->reldyn_offset);
+
+    write_to(ctx, ctx.buf + this->shdr.sh_offset, rel);
+  }
 }
 
 template <typename E>
-void OutputSection<E>::write_to(Context<E> &ctx, u8 *buf) {
+void OutputSection<E>::write_to(Context<E> &ctx, u8 *buf, ElfRel<E> *rel) {
+  // Copy section contents to an output file.
   tbb::parallel_for((i64)0, (i64)members.size(), [&](i64 i) {
-    // Copy section contents to an output file.
     InputSection<E> &isec = *members[i];
     isec.write_to(ctx, buf + isec.offset);
 
@@ -983,10 +984,44 @@ void OutputSection<E>::write_to(Context<E> &ctx, u8 *buf) {
     }
   });
 
+  // Emit range extension thunks.
   if constexpr (needs_thunk<E>) {
     tbb::parallel_for_each(thunks, [&](std::unique_ptr<Thunk<E>> &thunk) {
       thunk->copy_buf(ctx);
     });
+  }
+
+  // Emit dynamic relocations.
+  for (AbsRel<E> &r : abs_rels) {
+    Word<E> *loc = (Word<E> *)(buf + r.isec->offset + r.offset);
+    u64 addr = this->shdr.sh_addr + r.isec->offset + r.offset;
+
+    switch (r.kind) {
+    case ABS_REL_NONE:
+    case ABS_REL_RELR:
+      *loc = r.sym->get_addr(ctx) + r.addend;
+      break;
+    case ABS_REL_BASEREL: {
+      u64 val = r.sym->get_addr(ctx) + r.addend;
+      *rel++ = ElfRel<E>(addr, E::R_RELATIVE, 0, val);
+      if (ctx.arg.apply_dynamic_relocs)
+        *loc = val;
+      break;
+    }
+    case ABS_REL_IFUNC:
+      if constexpr (supports_ifunc<E>) {
+        u64 val = r.sym->get_addr(ctx, NO_PLT) + r.addend;
+        *rel++ = ElfRel<E>(addr, E::R_IRELATIVE, 0, val);
+        if (ctx.arg.apply_dynamic_relocs)
+          *loc = val;
+      }
+      break;
+    case ABS_REL_DYNREL:
+      *rel++ = ElfRel<E>(addr, E::R_ABS, r.sym->get_dynsym_idx(ctx), r.addend);
+      if (ctx.arg.apply_dynamic_relocs)
+        *loc = r.addend;
+      break;
+    }
   }
 }
 
@@ -1039,35 +1074,92 @@ static std::vector<u64> encode_relr(std::span<u64> pos) {
 }
 
 template <typename E>
-void OutputSection<E>::construct_relr(Context<E> &ctx) {
-  if (!ctx.arg.pic)
-    return;
-  if (!(this->shdr.sh_flags & SHF_ALLOC))
-    return;
-  if (this->shdr.sh_addralign % sizeof(Word<E>))
-    return;
+static AbsRelKind get_abs_rel_kind(Context<E> &ctx, Symbol<E> &sym) {
+  if (sym.is_ifunc())
+    return sym.is_pde_ifunc(ctx) ? ABS_REL_NONE : ABS_REL_IFUNC;
 
-  // Skip it if it is a text section because .text doesn't usually
-  // contain any dynamic relocations.
-  if (this->shdr.sh_flags & SHF_EXECINSTR)
-    return;
+  if (sym.is_absolute())
+    return ABS_REL_NONE;
 
-  // Collect base relocations
-  std::vector<std::vector<u64>> shards(members.size());
+  // True if the symbol's address is in the output file.
+  if (!sym.is_imported || (sym.flags & NEEDS_CPLT) || (sym.flags & NEEDS_COPYREL))
+    return ctx.arg.pic ? ABS_REL_BASEREL : ABS_REL_NONE;
 
+  return ABS_REL_DYNREL;
+}
+
+// Scan word-size absolute relocations (e.g. R_X86_64_64). This is
+// separated from scan_relocations() because only such relocations can
+// be promoted to dynamic relocations.
+template <typename E>
+void OutputSection<E>::scan_abs_relocations(Context<E> &ctx) {
+  std::vector<std::vector<AbsRel<E>>> shards(members.size());
+
+  // Collect all word-size absolute relocations
   tbb::parallel_for((i64)0, (i64)members.size(), [&](i64 i) {
-    InputSection<E> &isec = *members[i];
-
-    if (isec.shdr().sh_addralign % sizeof(Word<E>) == 0)
-      for (const ElfRel<E> &r : isec.get_rels(ctx))
-        if (r.r_type == E::R_ABS && r.r_offset % sizeof(Word<E>) == 0)
-          if (Symbol<E> &sym = *isec.file.symbols[r.r_sym];
-              !sym.is_ifunc() && !sym.is_absolute() && !sym.is_imported)
-            shards[i].push_back(isec.offset + r.r_offset);
+    InputSection<E> *isec = members[i];
+    for (const ElfRel<E> &r : isec->get_rels(ctx))
+      if (r.r_type == E::R_ABS)
+        shards[i].emplace_back(isec, r.r_offset, isec->file.symbols[r.r_sym],
+                               get_addend(*isec, r));
   });
 
-  // Compress them
-  std::vector<u64> pos = flatten(shards);
+  abs_rels = flatten(shards);
+
+  // We can sometimes avoid creating dynamic relocations in read-only
+  // sections by promoting symbols to canonical PLT or copy relocations.
+  if (!ctx.arg.pic && !(this->shdr.sh_flags & SHF_WRITE))
+    for (AbsRel<E> &r : abs_rels)
+      if (Symbol<E> &sym = *r.sym;
+          sym.is_imported && !sym.is_absolute())
+        sym.flags |= (sym.get_type() == STT_FUNC) ? NEEDS_CPLT : NEEDS_COPYREL;
+
+  // Now we can compute whether they need to be promoted to dynamic
+  // relocations or not.
+  for (AbsRel<E> &r : abs_rels)
+    r.kind = get_abs_rel_kind(ctx, *r.sym);
+
+  // If we have a relocation against a read-only section, we need to
+  // set the DT_TEXTREL flag for the loader.
+  for (AbsRel<E> &r : abs_rels) {
+    if (r.kind != ABS_REL_NONE && !(r.isec->shdr().sh_flags & SHF_WRITE)) {
+      if (ctx.arg.z_text) {
+        Error(ctx) << *r.isec << ": relocation at offset 0x"
+                   << std::hex << r.offset << " against symbol `"
+                   << *r.sym << "' can not be used; recompile with -fPIC";
+      } else if (ctx.arg.warn_textrel) {
+        Warn(ctx) << *r.isec << ": relocation against symbol `" << *r.sym
+                  << "' in read-only section";
+      }
+      ctx.has_textrel = true;
+    }
+  }
+
+  // If --pack-dyn-relocs=relr is enabled, base relocations are put into
+  // .relr.dyn.
+  if (ctx.arg.pack_dyn_relocs_relr)
+    for (AbsRel<E> &r : abs_rels)
+      if (r.kind == ABS_REL_BASEREL &&
+          r.isec->shdr().sh_addralign % sizeof(Word<E>) == 0 &&
+          r.offset % sizeof(Word<E>) == 0)
+        r.kind = ABS_REL_RELR;
+}
+
+template <typename E>
+i64 OutputSection<E>::get_reldyn_size(Context<E> &ctx) const {
+  i64 n = 0;
+  for (const AbsRel<E> &r : abs_rels)
+    if (r.kind != ABS_REL_NONE && r.kind != ABS_REL_RELR)
+      n++;
+  return n;
+}
+
+template <typename E>
+void OutputSection<E>::construct_relr(Context<E> &ctx) {
+  std::vector<u64> pos;
+  for (const AbsRel<E> &r : abs_rels)
+    if (r.kind == ABS_REL_RELR)
+      pos.push_back(r.isec->offset + r.offset);
   this->relr = encode_relr<E>(pos);
 }
 
@@ -1388,13 +1480,10 @@ void GotSection<E>::copy_buf(Context<E> &ctx) {
 
 template <typename E>
 void GotSection<E>::construct_relr(Context<E> &ctx) {
-  assert(ctx.arg.pack_dyn_relocs_relr);
-
   std::vector<u64> pos;
   for (GotEntry<E> &ent : get_got_entries(ctx))
     if (ent.is_relr(ctx))
       pos.push_back(ent.idx * sizeof(Word<E>));
-
   this->relr = encode_relr<E>(pos);
 }
 
@@ -2104,11 +2193,11 @@ void MergedSection<E>::compute_section_size(Context<E> &ctx) {
 
 template <typename E>
 void MergedSection<E>::copy_buf(Context<E> &ctx) {
-  write_to(ctx, ctx.buf + this->shdr.sh_offset);
+  write_to(ctx, ctx.buf + this->shdr.sh_offset, nullptr);
 }
 
 template <typename E>
-void MergedSection<E>::write_to(Context<E> &ctx, u8 *buf) {
+void MergedSection<E>::write_to(Context<E> &ctx, u8 *buf, ElfRel<E> *rel) {
   i64 shard_size = map.nbuckets / map.NUM_SHARDS;
 
   tbb::parallel_for((i64)0, map.NUM_SHARDS, [&](i64 i) {
@@ -2746,7 +2835,7 @@ CompressedSection<E>::CompressedSection(Context<E> &ctx, Chunk<E> &chunk) {
   this->uncompressed_data.resize(chunk.shdr.sh_size);
   u8 *buf = this->uncompressed_data.data();
 
-  chunk.write_to(ctx, buf);
+  chunk.write_to(ctx, buf, nullptr);
 
   switch (ctx.arg.compress_debug_sections) {
   case COMPRESS_ZLIB:

src/arch-alpha.cc
@@ -81,11 +81,6 @@ template <>
 void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
-  ElfRel<E> *dynrel = nullptr;
-  if (ctx.reldyn)
-    dynrel = (ElfRel<E> *)(ctx.buf + ctx.reldyn->shdr.sh_offset +
-                           file.reldyn_offset + this->reldyn_offset);
-
   for (i64 i = 0; i < rels.size(); i++) {
     const ElfRel<E> &rel = rels[i];
     if (rel.r_type == R_NONE)
@@ -102,9 +97,6 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
     u64 GP = ctx.got->shdr.sh_addr + 0x8000;
 
     switch (rel.r_type) {
-    case R_ALPHA_REFQUAD:
-      apply_dyn_absrel(ctx, sym, rel, loc, S, A, P, &dynrel);
-      break;
     case R_ALPHA_GPREL32:
       *(ul32 *)loc = S + A - GP;
       break;
@@ -151,6 +143,7 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
     case R_ALPHA_TPRELLO:
       *(ul16 *)loc = S + A - ctx.tp_addr;
       break;
+    case R_ALPHA_REFQUAD:
     case R_ALPHA_LITUSE:
     case R_ALPHA_HINT:
       break;
@@ -202,8 +195,6 @@ void InputSection<E>::apply_reloc_nonalloc(Context<E> &ctx, u8 *base) {
 template <>
 void InputSection<E>::scan_relocations(Context<E> &ctx) {
   assert(shdr().sh_flags & SHF_ALLOC);
-
-  this->reldyn_offset = file.num_dynrel * sizeof(ElfRel<E>);
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
   for (i64 i = 0; i < rels.size(); i++) {
@@ -217,9 +208,6 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
       Error(ctx) << sym << ": GNU ifunc symbol is not supported on Alpha";
 
     switch (rel.r_type) {
-    case R_ALPHA_REFQUAD:
-      scan_dyn_absrel(ctx, sym, rel);
-      break;
     case R_ALPHA_LITERAL:
       if (rel.r_addend)
         ctx.extra.got->add_symbol(sym, rel.r_addend);
@@ -246,6 +234,7 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_ALPHA_TPRELLO:
       check_tlsle(ctx, sym, rel);
       break;
+    case R_ALPHA_REFQUAD:
     case R_ALPHA_GPREL32:
     case R_ALPHA_LITUSE:
     case R_ALPHA_GPDISP:

src/arch-arm32.cc
@@ -256,11 +256,6 @@ template <>
 void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
-  ElfRel<E> *dynrel = nullptr;
-  if (ctx.reldyn)
-    dynrel = (ElfRel<E> *)(ctx.buf + ctx.reldyn->shdr.sh_offset +
-                           file.reldyn_offset + this->reldyn_offset);
-
   auto get_tls_trampoline_addr = [&, i = 0](u64 addr) mutable {
     for (; i < output_section->thunks.size(); i++) {
       i64 disp = output_section->shdr.sh_addr + output_section->thunks[i]->offset -
@@ -299,7 +294,6 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
     switch (rel.r_type) {
     case R_ARM_ABS32:
     case R_ARM_TARGET1:
-      apply_dyn_absrel(ctx, sym, rel, loc, S, A, P, &dynrel);
       break;
     case R_ARM_REL32:
       *(ul32 *)loc = S + A - P;
@@ -586,8 +580,6 @@ void InputSection<E>::apply_reloc_nonalloc(Context<E> &ctx, u8 *base) {
 template <>
 void InputSection<E>::scan_relocations(Context<E> &ctx) {
   assert(shdr().sh_flags & SHF_ALLOC);
-
-  this->reldyn_offset = file.num_dynrel * sizeof(ElfRel<E>);
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
   // Scan relocations
@@ -602,12 +594,6 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
       sym.flags |= NEEDS_GOT | NEEDS_PLT;
 
     switch (rel.r_type) {
-    case R_ARM_ABS32:
-    case R_ARM_MOVT_ABS:
-    case R_ARM_THM_MOVT_ABS:
-    case R_ARM_TARGET1:
-      scan_dyn_absrel(ctx, sym, rel);
-      break;
     case R_ARM_MOVW_ABS_NC:
     case R_ARM_THM_MOVW_ABS_NC:
       scan_absrel(ctx, sym, rel);
@@ -646,6 +632,10 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_ARM_TLS_LE32:
       check_tlsle(ctx, sym, rel);
       break;
+    case R_ARM_ABS32:
+    case R_ARM_MOVT_ABS:
+    case R_ARM_THM_MOVT_ABS:
+    case R_ARM_TARGET1:
     case R_ARM_REL32:
     case R_ARM_BASE_PREL:
     case R_ARM_GOTOFF32:
@@ -762,7 +752,7 @@ std::vector<u8> Arm32ExidxSection::get_contents(Context<E> &ctx) {
   std::vector<u8> buf(output_section.shdr.sh_size);
 
   output_section.shdr.sh_addr = this->shdr.sh_addr;
-  output_section.write_to(ctx, buf.data());
+  output_section.write_to(ctx, buf.data(), nullptr);
 
   // .ARM.exidx records consists of a signed 31-bit relative address
   // and a 32-bit value. The relative address indicates the start

src/arch-arm64.cc
@@ -145,11 +145,6 @@ template <>
 void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
-  ElfRel<E> *dynrel = nullptr;
-  if (ctx.reldyn)
-    dynrel = (ElfRel<E> *)(ctx.buf + ctx.reldyn->shdr.sh_offset +
-                           file.reldyn_offset + this->reldyn_offset);
-
   for (i64 i = 0; i < rels.size(); i++) {
     const ElfRel<E> &rel = rels[i];
     if (rel.r_type == R_NONE)
@@ -173,7 +168,6 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
 
     switch (rel.r_type) {
     case R_AARCH64_ABS64:
-      apply_dyn_absrel(ctx, sym, rel, loc, S, A, P, &dynrel);
       break;
     case R_AARCH64_LDST8_ABS_LO12_NC:
     case R_AARCH64_ADD_ABS_LO12_NC:
@@ -490,8 +484,6 @@ void InputSection<E>::apply_reloc_nonalloc(Context<E> &ctx, u8 *base) {
 template <>
 void InputSection<E>::scan_relocations(Context<E> &ctx) {
   assert(shdr().sh_flags & SHF_ALLOC);
-
-  this->reldyn_offset = file.num_dynrel * sizeof(ElfRel<E>);
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
   // Scan relocations
@@ -507,9 +499,6 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
       sym.flags |= NEEDS_GOT | NEEDS_PLT;
 
     switch (rel.r_type) {
-    case R_AARCH64_ABS64:
-      scan_dyn_absrel(ctx, sym, rel);
-      break;
     case R_AARCH64_MOVW_UABS_G3:
       scan_absrel(ctx, sym, rel);
       break;
@@ -569,6 +558,7 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_AARCH64_TLSLE_ADD_TPREL_LO12_NC:
       check_tlsle(ctx, sym, rel);
       break;
+    case R_AARCH64_ABS64:
     case R_AARCH64_ADD_ABS_LO12_NC:
     case R_AARCH64_ADR_PREL_LO21:
     case R_AARCH64_CONDBR19:

src/arch-i386.cc
@@ -286,11 +286,6 @@ template <>
 void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
-  ElfRel<E> *dynrel = nullptr;
-  if (ctx.reldyn)
-    dynrel = (ElfRel<E> *)(ctx.buf + ctx.reldyn->shdr.sh_offset +
-                           file.reldyn_offset + this->reldyn_offset);
-
   for (i64 i = 0; i < rels.size(); i++) {
     const ElfRel<E> &rel = rels[i];
     if (rel.r_type == R_NONE)
@@ -322,7 +317,6 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
       *(ul16 *)loc = S + A;
       break;
     case R_386_32:
-      apply_dyn_absrel(ctx, sym, rel, loc, S, A, P, &dynrel);
       break;
     case R_386_PC8:
       check(S + A - P, -(1 << 7), 1 << 7);
@@ -520,8 +514,6 @@ void InputSection<E>::apply_reloc_nonalloc(Context<E> &ctx, u8 *base) {
 template <>
 void InputSection<E>::scan_relocations(Context<E> &ctx) {
   assert(shdr().sh_flags & SHF_ALLOC);
-
-  this->reldyn_offset = file.num_dynrel * sizeof(ElfRel<E>);
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
   // Scan relocations
@@ -550,9 +542,6 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_386_8:
     case R_386_16:
       scan_absrel(ctx, sym, rel);
-      break;
-    case R_386_32:
-      scan_dyn_absrel(ctx, sym, rel);
       break;
     case R_386_PC8:
     case R_386_PC16:
@@ -602,6 +591,7 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_386_TLS_LE:
       check_tlsle(ctx, sym, rel);
       break;
+    case R_386_32:
     case R_386_GOTOFF:
     case R_386_TLS_LDO_32:
     case R_386_SIZE32:

src/arch-loongarch.cc
@@ -266,11 +266,6 @@ template <>
 void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
-  ElfRel<E> *dynrel = nullptr;
-  if (ctx.reldyn)
-    dynrel = (ElfRel<E> *)(ctx.buf + ctx.reldyn->shdr.sh_offset +
-                           file.reldyn_offset + this->reldyn_offset);
-
   auto get_r_delta = [&](i64 idx) {
     return extra.r_deltas.empty() ? 0 : extra.r_deltas[idx];
   };
@@ -329,12 +324,6 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
     case R_LARCH_32:
       if constexpr (E::is_64)
         *(ul32 *)loc = S + A;
-      else
-        apply_dyn_absrel(ctx, sym, rel, loc, S, A, P, &dynrel);
-      break;
-    case R_LARCH_64:
-      assert(E::is_64);
-      apply_dyn_absrel(ctx, sym, rel, loc, S, A, P, &dynrel);
       break;
     case R_LARCH_B16:
       check_branch(S + A - P, -(1 << 17), 1 << 17);
@@ -661,6 +650,7 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
         set_rj(loc, 2);
       break;
     }
+    case R_LARCH_64:
     case R_LARCH_TLS_LE_ADD_R:
       break;
     default:
@@ -762,8 +752,6 @@ void InputSection<E>::apply_reloc_nonalloc(Context<E> &ctx, u8 *base) {
 template <>
 void InputSection<E>::scan_relocations(Context<E> &ctx) {
   assert(shdr().sh_flags & SHF_ALLOC);
-
-  this->reldyn_offset = file.num_dynrel * sizeof(ElfRel<E>);
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
   // Scan relocations
@@ -787,12 +775,6 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_LARCH_32:
       if constexpr (E::is_64)
         scan_absrel(ctx, sym, rel);
-      else
-        scan_dyn_absrel(ctx, sym, rel);
-      break;
-    case R_LARCH_64:
-      assert(E::is_64);
-      scan_dyn_absrel(ctx, sym, rel);
       break;
     case R_LARCH_B26:
     case R_LARCH_PCALA_HI20:
@@ -829,6 +811,7 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_LARCH_TLS_DESC_CALL:
       scan_tlsdesc(ctx, sym);
       break;
+    case R_LARCH_64:
     case R_LARCH_B16:
     case R_LARCH_B21:
     case R_LARCH_ABS_HI20:

src/arch-m68k.cc
@@ -78,11 +78,6 @@ template <>
 void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
-  ElfRel<E> *dynrel = nullptr;
-  if (ctx.reldyn)
-    dynrel = (ElfRel<E> *)(ctx.buf + ctx.reldyn->shdr.sh_offset +
-                           file.reldyn_offset + this->reldyn_offset);
-
   for (i64 i = 0; i < rels.size(); i++) {
     const ElfRel<E> &rel = rels[i];
     if (rel.r_type == R_NONE)
@@ -126,7 +121,6 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
 
     switch (rel.r_type) {
     case R_68K_32:
-      apply_dyn_absrel(ctx, sym, rel, loc, S, A, P, &dynrel);
       break;
     case R_68K_16:
       write16(S + A);
@@ -251,8 +245,6 @@ void InputSection<E>::apply_reloc_nonalloc(Context<E> &ctx, u8 *base) {
 template <>
 void InputSection<E>::scan_relocations(Context<E> &ctx) {
   assert(shdr().sh_flags & SHF_ALLOC);
-
-  this->reldyn_offset = file.num_dynrel * sizeof(ElfRel<E>);
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
   for (i64 i = 0; i < rels.size(); i++) {
@@ -266,9 +258,6 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
       Error(ctx) << sym << ": GNU ifunc symbol is not supported on m68k";
 
     switch (rel.r_type) {
-    case R_68K_32:
-      scan_dyn_absrel(ctx, sym, rel);
-      break;
     case R_68K_16:
     case R_68K_8:
       scan_absrel(ctx, sym, rel);
@@ -312,6 +301,7 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_68K_TLS_LE8:
       check_tlsle(ctx, sym, rel);
       break;
+    case R_68K_32:
     case R_68K_TLS_LDO32:
     case R_68K_TLS_LDO16:
     case R_68K_TLS_LDO8:

src/arch-ppc32.cc
@@ -148,11 +148,6 @@ template <>
 void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
-  ElfRel<E> *dynrel = nullptr;
-  if (ctx.reldyn)
-    dynrel = (ElfRel<E> *)(ctx.buf + ctx.reldyn->shdr.sh_offset +
-                           file.reldyn_offset + this->reldyn_offset);
-
   u64 GOT2 = file.extra.got2 ? file.extra.got2->get_addr() : 0;
 
   for (i64 i = 0; i < rels.size(); i++) {
@@ -170,10 +165,6 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
     u64 GOT = ctx.got->shdr.sh_addr;
 
     switch (rel.r_type) {
-    case R_PPC_ADDR32:
-    case R_PPC_UADDR32:
-      apply_dyn_absrel(ctx, sym, rel, loc, S, A, P, &dynrel);
-      break;
     case R_PPC_ADDR14:
       *(ub32 *)loc |= bits(S + A, 15, 2) << 2;
       break;
@@ -275,6 +266,8 @@ void InputSection<E>::apply_reloc_alloc(Context<E> &ctx, u8 *base) {
     case R_PPC_GOT_TPREL16:
       *(ub16 *)loc = sym.get_gottp_addr(ctx) - GOT;
       break;
+    case R_PPC_ADDR32:
+    case R_PPC_UADDR32:
     case R_PPC_TLS:
     case R_PPC_TLSGD:
     case R_PPC_TLSLD:
@@ -323,8 +316,6 @@ void InputSection<E>::apply_reloc_nonalloc(Context<E> &ctx, u8 *base) {
 template <>
 void InputSection<E>::scan_relocations(Context<E> &ctx) {
   assert(shdr().sh_flags & SHF_ALLOC);
-
-  this->reldyn_offset = file.num_dynrel * sizeof(ElfRel<E>);
   std::span<const ElfRel<E>> rels = get_rels(ctx);
 
   // Scan relocations
@@ -339,10 +330,6 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
       sym.flags |= NEEDS_GOT | NEEDS_PLT;
 
     switch (rel.r_type) {
-    case R_PPC_ADDR32:
-    case R_PPC_UADDR32:
-      scan_dyn_absrel(ctx, sym, rel);
-      break;
     case R_PPC_ADDR14:
     case R_PPC_ADDR16:
     case R_PPC_UADDR16:
@@ -391,6 +378,8 @@ void InputSection<E>::scan_relocations(Context<E> &ctx) {
     case R_PPC_TPREL16_HA:
       check_tlsle(ctx, sym, rel);
       break;
+    case R_PPC_ADDR32:
+    case R_PPC_UADDR32:
     case R_PPC_LOCAL24PC:
     case R_PPC_TLS:
     case R_PPC_TLSGD:

commit 186272a425a36d12fd25897aa3628631f3df0099

Handle R_ARM_TARGET1 as a synonym for R_ARM_ABS32
This fixes a regression introduced in 431fc39db00afb6b5dc9ac7260cd49823fa19160.

Fixes https://github.com/rui314/mold/issues/1351

src/output-chunks.cc
@@ -1109,11 +1109,21 @@ template <typename E>
 void OutputSection<E>::scan_abs_relocations(Context<E> &ctx) {
   std::vector<std::vector<AbsRel<E>>> shards(members.size());
 
+  auto is_absrel = [](const ElfRel<E> &r) {
+    // R_ARM_TARGET1 is typically used for entries in .init_array and can
+    // be interpreted as REL32 or ABS32 depending on the target.
+    // All targets we support handle it as if it were a synonym for ABS32.
+    if constexpr (is_arm32<E>)
+      if (r.r_type == R_ARM_TARGET1)
+        return true;
+    return r.r_type == E::R_ABS;
+  };
+
   // Collect all word-size absolute relocations
   tbb::parallel_for((i64)0, (i64)members.size(), [&](i64 i) {
     InputSection<E> *isec = members[i];
     for (const ElfRel<E> &r : isec->get_rels(ctx))
-      if (r.r_type == E::R_ABS)
+      if (is_absrel(r))
         shards[i].push_back(AbsRel<E>{isec, r.r_offset, isec->file.symbols[r.r_sym],
                                       get_addend(*isec, r)});
   });

test/arch-arm-target1.sh
@@ -1,0 +1,19 @@
+#!/bin/bash
+. $(dirname $0)/common.inc
+
+cat <<EOF | $CC -c -o $t/a.o -xassembler -
+.globl foo
+.data
+foo:
+.word bar(TARGET1)
+EOF
+
+cat <<EOF | $CC -fPIC -c -o $t/b.o -xc -
+#include <stdio.h>
+extern char *foo;
+char bar[] = "Hello world";
+int main() { printf("%s\n", foo); }
+EOF
+
+$CC -B. -o $t/exe -pie $t/a.o $t/b.o
+$QEMU $t/exe | grep -q 'Hello world'