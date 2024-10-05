40e7b983d6ca82f18823fc49f2366751eee5c64d
Rui Ueyama <ruiu@cs.stanford.edu>
2024-10-05 14:44:03 +0900

    Fix CI

--- test/compress-debug-sections.sh
+++ test/compress-debug-sections.sh
@@ -3,14 +3,11 @@
 
 cat <<EOF | $CC -c -g -o $t/a.o -xc -
 #include <stdio.h>
-
-int main() {
-  printf("Hello world\n");
-  return 0;
-}
+int main() { printf("Hello world\n"); }
 EOF
 
 $CC -B. -o $t/exe $t/a.o -Wl,--compress-debug-sections=zlib
-readelf -tW $t/exe > $t/log
-grep -F -A2 '.debug_info' $t/log | tr '\n' ' ' | grep -qw 'COMPRESSED'
-grep -F -A2 '.debug_str' $t/log | tr '\n' ' ' | grep -qw 'COMPRESSED'
+
+readelf -WS $t/exe > $t/log
+grep -q '\.debug_info .* [Cx] ' $t/log
+grep -q '\.debug_str .* MS[Cx] ' $t/log
