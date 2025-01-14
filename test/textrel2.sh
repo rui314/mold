7fc2a6e93569dea2979512b411e9fc10364e266b
Rui Ueyama <ruiu@cs.stanford.edu>
2025-01-14 13:25:32 +0900

    Fix CI

--- test/textrel2.sh
+++ test/textrel2.sh
@@ -20,6 +20,7 @@ $CC -B. -o $t/exe1 $t/a.o -pie
 $QEMU $t/exe1 | grep -q 'Hello world'
 
 $CC -o $t/exe2 $t/a.o -pie -Wl,-z,pack-relative-relocs 2> /dev/null || skip
+readelf -WS $t/exe2 | grep -Fq .relr.dyn || skip
 $QEMU $t/exe2 2> /dev/null | grep -q 'Hello world' || skip
 
 $CC -B. -o $t/exe3 $t/a.o -pie -Wl,-z,pack-relative-relocs
