6e50f3a5e7a646bd7bce442cd9331bfa017a0af7
Rui Ueyama <ruiu@cs.stanford.edu>
2025-02-09 11:38:48 +0900

    Remove a redundant test

--- test/z-unknown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. $(dirname $0)/common.inc
-
-not ./mold -z no-such-opt |& grep 'unknown command line option: -z no-such-opt'
-not ./mold -zno-such-opt |& grep 'unknown command line option: -zno-such-opt'
