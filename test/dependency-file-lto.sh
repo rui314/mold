f2ac49824c479a41b2c38bfccd8b268423ed3dd3
Rui Ueyama <ruiu@cs.stanford.edu>
2025-03-03 10:45:35 +0900

    Fix CI

--- test/dependency-file-lto.sh
+++ test/dependency-file-lto.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 . $(dirname $0)/common.inc
 
+test_cflags -flto || skip
+
 cat <<EOF | $CC -flto -o $t/a.o -c -xc -
 #include <stdio.h>
 int main() {
@@ -13,4 +15,3 @@ $CC -B. -flto -o $t/exe $t/a.o -Wl,-dependency-file=$t/dep
 grep '/exe:.*/a.o ' $t/dep
 grep '/a.o:$' $t/dep
 not grep '^/tmp' $t/dep
-
