b7c8a843b28dc103489fc69cb32afb65e02d933c
Rui Ueyama <ruiu@cs.stanford.edu>
2025-04-03 16:57:15 +0900

    Ignore --mmap-output-file
    For the sake of completeness, --mmap-output-file is ignored just like
    --no-mmap-output-file is.

--- src/cmdline.cc
+++ src/cmdline.cc
@@ -1348,6 +1348,7 @@ std::vector<std::string> parse_nonpositional_args(Context<E> &ctx) {
     } else if (read_z_arg("common-page-size")) {
     } else if (read_flag("no-keep-memory")) {
     } else if (read_arg("max-cache-size")) {
+    } else if (read_flag("mmap-output-file")) {
     } else if (read_flag("no-mmap-output-file")) {
     } else if (read_arg("version-script")) {
       version_scripts.push_back(arg);
