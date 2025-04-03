9b77a7ea86367b46e102b960e3fe384533af87d3
Rui Ueyama <ruiu@cs.stanford.edu>
2025-04-03 16:02:20 +0900

    Fix CI

--- test/audit.sh
+++ test/audit.sh
@@ -13,17 +13,3 @@ readelf --dynamic $t/exe2 | grep -F 'Audit library: [foo]'
 
 $CC -B. -o $t/exe3 $t/a.o -Wl,--audit=foo -Wl,--audit=bar
 readelf --dynamic $t/exe3 | grep -F 'Audit library: [foo:bar]'
-
-
-cat <<'EOF' | $CC -B. -shared -o $t/b.so -xc - -fPIC
-#include <stdio.h>
-unsigned la_version(unsigned v) {
-  fprintf(stderr, "version=%d\n", v);
-  return 0;
-}
-EOF
-
-LD_AUDIT=$t/b.so $QEMU $t/exe1 |& grep 'version=' || skip
-
-$CC -B. -o $t/exe4 $t/a.o -Wl,--audit=$t/b.so
-$QEMU $t/exe4 |& grep 'version='
