46634687ad469e03fef13b4002aeba3bf8935003
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-04-24 11:21:41 -0400

    Print MOLD_VERSION after GNU ld compatibility string too.
    As per discussion in https://github.com/rui314/mold/discussions/784#discussioncomment-12936908

--- src/config.cc
+++ src/config.cc
@@ -4,9 +4,9 @@ namespace mold {
 
 std::string get_mold_version() {
   if (mold_git_hash.empty())
-    return "mold "s + MOLD_VERSION + " (compatible with GNU ld)";
+    return "mold "s + MOLD_VERSION + " (compatible with GNU ld " + MOLD_VERSION + ")";
   return "mold "s + MOLD_VERSION + " (" + mold_git_hash +
-         "; compatible with GNU ld)";
+         "; compatible with GNU ld " + MOLD_VERSION + ")";
 }
 
 } // namespace mold
