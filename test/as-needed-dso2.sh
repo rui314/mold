af36625a02957c9dc717954b8fbb5c28e1d7ac0e
Rui Ueyama <ruiu@cs.stanford.edu>
2025-05-16 15:36:32 +0900

    Fix bug where --as-needed did not work if --allow-shlib-undefined is given

--- test/as-needed-dso2.sh
+++ test/as-needed-dso2.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 . $(dirname $0)/common.inc
 
-cat <<EOF | $CC -c -fPIC -o $t/a.o -xc -
-int foo() {
-  return 0;
-}
+cat <<EOF | $CC -o $t/libfoo.so -shared -fPIC -Wl,-soname,libfoo.so -xc -
+int fn1() { return 42; }
 EOF
 
-cat <<EOF | $CC -c -fPIC -o $t/b.o -xc -
-__attribute__((weak)) int foo();
-
-int bar() {
-  if (foo) return foo();
-  return 0;
-}
+cat <<EOF | $CC -o $t/libbar.so -shared -fPIC -Wl,-soname,libbar.so -xc - -L$t -lfoo
+int fn1();
+int fn2() { return fn1(); }
 EOF
 
-cat <<EOF | $CC -xc -c -o $t/c.o -
-int bar();
-
-int main() {
-  return bar();
-}
+cat <<EOF | $CC -o $t/libbaz.so -shared -fPIC -Wl,-soname,libbaz.so -xc - -L$t -lbar
+int fn2();
+int fn3() { return fn2(); }
 EOF
 
-$CC -B. -shared -o $t/libfoo.so $t/a.o
-$CC -B. -shared -o $t/libbar.so $t/b.o
-$CC -B. -o $t/exe $t/c.o -L$t -Wl,--as-needed -lfoo -lbar
+cat <<EOF | $CC -o $t/a.o -c -xc -
+int fn3();
+int main() { fn3(); }
+EOF
 
-readelf --dynamic $t/exe > $t/log
-not grep libfoo.so $t/log
-grep libbar.so $t/log
+$CC -B. -o $t/exe $t/a.o -L$t -Wl,--as-needed -lbaz -lbar
+readelf -W --dynamic $t/exe > $t/log2
+
+not grep libfoo $t/log2
+not grep libbar $t/log2
+grep libbaz $t/log2
