6e27786f8685802ccea00246bb9258ef0f3a34c0
Rui Ueyama <ruiu@cs.stanford.edu>
2025-05-16 10:45:29 +0900

    Align TLS segment correctly
    This effectively reverts 5249edc57f8cfbf5c9a3821f82c71c9713f47cdf.

--- test/tls-large-alignment.sh
+++ test/tls-large-alignment.sh
@@ -18,14 +18,14 @@ extern _Thread_local int x;
 extern _Thread_local int y[];
 
 int main() {
-  printf("%d %d %d %d\n", x, y[0], y[1], y[2]);
+  printf("%lu %d %d %d %d\n", (unsigned long)&x % 256, x, y[0], y[1], y[2]);
 }
 EOF
 
 $CC -B. -shared -o $t/d.so $t/a.o $t/b.o
 
 $CC -B. -o $t/exe1 $t/a.o $t/b.o $t/c.o
-$QEMU $t/exe1 | grep '^42 1 2 3$'
+$QEMU $t/exe1 | grep '^0 42 1 2 3$'
 
 $CC -B. -o $t/exe2 $t/c.o $t/d.so
-$QEMU $t/exe2 | grep '^42 1 2 3$'
+$QEMU $t/exe2 | grep '^0 42 1 2 3$'
