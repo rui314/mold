1ce5b39f7e616c15e358157a661158ff31aeeab4
Rui Ueyama <ruiu@cs.stanford.edu>
2026-08-11 00:18:09 +0900

    Install loongarch64 build dependencies from the main Debian archive

--- dist.sh
+++ dist.sh
@@ -186,7 +186,7 @@ loongarch64)
   cat <<EOF | $image_build
 FROM mirror.gcr.io/loongarch64/debian:sid@sha256:0356df4e494bbb86bb469377a00789a5b42bbf67d5ff649a3f9721b745cbef77
 ENV DEBIAN_FRONTEND=noninteractive TZ=UTC
-RUN sed -i -e 's!http[^ ]*!http://snapshot.debian.org/archive/debian-ports/20250620T014755Z!g' /etc/apt/sources.list && \
+RUN echo 'deb http://deb.debian.org/debian sid main' > /etc/apt/sources.list && \
   echo 'Acquire::Retries "10"; Acquire::http::timeout "10"; Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/80-retries && \
   apt-get update && \
   apt-get install -y --no-install-recommends build-essential gcc-14 g++-14 clang-19 cmake git && \
